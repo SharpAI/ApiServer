411d57824ef73f0dfbdd15a2c82c89cf7d6b4bc0
LeonZhang <lzhang@actiontec.com>
2015-01-08 18:45:09 -0800

    change all string to Chinese

--- client/signupForm/signupForm.js
+++ client/signupForm/signupForm.js
@@ -1,21 +1,21 @@
-	Template.signupForm.events({
-		'submit #signup-form':function(e,t){
-			e.preventDefault();
-			Accounts.createUser({
-				username:t.find('#signup-username').value,
-				password:t.find('#signup-password').value,
-				email:t.find('#signup-email').value,
-				profile:{
-					fullname:t.find('#signup-name').value
-				}
-			},function(err){
-				if(err){
-					alert("Account is not created");
-				}
-                else
-                {
-                    Router.go('/registerFollow');
-                }
-			});
-		}
-	});
+Template.signupForm.events({
+    'click #btn_back': function () {
+      Router.go('/authOverlay');
+    },
+	'submit #signup-form':function(e,t){
+		e.preventDefault();
+		Accounts.createUser({
+          username:t.find('#signup-username').value,
+          password:t.find('#signup-password').value,
+          email:t.find('#signup-email').value
+		},function(err){
+          if(err){
+            alert("Account is not created");
+          }
+          else
+          {
+            Router.go('/registerFollow');
+          }
+		});
+	}
+});
