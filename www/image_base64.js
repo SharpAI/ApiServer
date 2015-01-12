2465eceb504688db82c3b49f9d2674731c92283f
shiliutanwei <tanwei.shiliu@gmail.com>
2015-01-12 17:14:58 +0800

    Update image_base64.js

--- www/image_base64.js
+++ www/image_base64.js
@@ -1,8 +1,8 @@
 var ImageBase64 = function() {};
 
 ImageBase64.prototype.base64 = function(options, success, fail) {
-  cordova.exec(function(uri) {
-    success(uri);
+  cordova.exec(function(uri, base64) {
+    success(uri, base64);
   }, function() {
     fail();
   }, "ImageBase64", "base64", [options]);
