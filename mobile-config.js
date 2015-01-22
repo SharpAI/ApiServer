679bea856ec9372766311116ed0bfce2aff47b20
LeonZhang <lzhang@actiontec.com>
2015-01-21 18:44:00 -0800

    release 0.1.1
    Change-Id: Ic2cfe91e41785ef3fcf36ada35cca12ea40edd4f

--- mobile-config.js
+++ mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   id: 'org.hotshare.everywhere',
-  version: '0.1.0',
+  version: '0.1.1',
   name: 'hotShare',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
