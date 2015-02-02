167b08f0b20a1d281cd41065e37ad21e8524f321
Simba Zhang <zzhang@actiontec.com>
2015-02-01 21:13:54 -0800

    Changed version number to 0.3.3.
    Change-Id: I255d6407a8ec739fcfea836bb0f57dffe0d2a597

--- mobile-config.js
+++ mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   id: 'org.hotshare.everywhere',
-  version: '0.3.2',
+  version: '0.3.3',
   name: 'hotShare',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
