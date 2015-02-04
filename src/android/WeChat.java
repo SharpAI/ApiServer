4a3ca52a6b517de0df53bf151dd4811fe7a77800
LeonZhang <lzhang@actiontec.com>
2015-02-04 11:55:40 -0800

    scale thumb

--- src/android/WeChat.java
+++ src/android/WeChat.java
@@ -13,10 +13,8 @@ import java.net.URLConnection;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
-
 import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.CordovaInterface;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -200,11 +198,20 @@ public class WeChat extends CordovaPlugin {
                 message.description = messageOptions.getString("description");
             }
 
+            Bitmap thumb = null;
             if (!messageOptions.isNull("thumbData")) {
                 String thumbData = messageOptions.getString("thumbData");
                 //message.thumbData = getHtmlByteArray(thumbData);
                 thumbData = thumbData.replaceAll("file://", "");
-                message.thumbData = readFromFile(thumbData, 0, (int) new File(thumbData).length());
+                try {
+                    thumb = BitmapFactory.decodeStream(new FileInputStream(thumbData));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                thumb = centerSquareScaleBitmap(thumb, 200);
+                //message.thumbData = readFromFile(thumbData, 0, (int) new File(thumbData).length());
+                message.setThumbImage(thumb);
+                thumb.recycle();
                 //message.thumbData = getHtmlByteArray(thumbData);
             }
         } else if (text != null) {
@@ -277,6 +284,45 @@ public class WeChat extends CordovaPlugin {
                         e.printStackTrace();
                 }
                 return b;
+        }
+
+        /**
+        *
+        * @param bitmap
+        * 原图
+        * @param edgeLength
+        * 希望得到的正方形部分的边长
+        * @return 缩放截取正中部分后的位图。
+        */
+        public static Bitmap centerSquareScaleBitmap(Bitmap bitmap, int edgeLength) {
+        if (null == bitmap || edgeLength <= 0) {
+        return null;
+        }
+        Bitmap result = bitmap;
+        int widthOrg = bitmap.getWidth();
+        int heightOrg = bitmap.getHeight();
+        if (widthOrg > edgeLength && heightOrg > edgeLength) {
+        // 压缩到一个最小长度是edgeLength的bitmap
+        int longerEdge = (int) (edgeLength * Math.max(widthOrg, heightOrg) / Math.min(widthOrg, heightOrg));
+        int scaledWidth = widthOrg > heightOrg ? longerEdge : edgeLength;
+        int scaledHeight = widthOrg > heightOrg ? edgeLength : longerEdge;
+        Bitmap scaledBitmap;
+        try {
+        scaledBitmap = Bitmap.createScaledBitmap(bitmap, scaledWidth, scaledHeight, true);
+        } catch (Exception e) {
+        return null;
+        }
+        // 从图中截取正中间的正方形部分。
+        int xTopLeft = (scaledWidth - edgeLength) / 2;
+        int yTopLeft = (scaledHeight - edgeLength) / 2;
+        try {
+        result = Bitmap.createBitmap(scaledBitmap, xTopLeft, yTopLeft, edgeLength, edgeLength);
+        scaledBitmap.recycle();
+        } catch (Exception e) {
+        return null;
+        }
+        }
+        return result;
         }
 
     public static byte[] getHtmlByteArray(final String url) {
