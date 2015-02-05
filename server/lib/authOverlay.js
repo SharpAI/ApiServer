91a8b6badb395ab1fa3a5a707e44e07762f1b6c6
LeonZhang <lzhang@actiontec.com>
2015-02-05 09:42:56 -0800

    update appId
    Change-Id: I7a1a4b0cae83dde23b4d15d00d6ed05625fcdbe5

--- server/lib/authOverlay.js
+++ server/lib/authOverlay.js
@@ -13,18 +13,18 @@ if (Meteor.isServer) {
 
     Accounts.loginServiceConfiguration.insert({
       service: 'weibo',
-      clientId: '123490452',
-      secret: 'f69795795d96fe8518a4f0bc14d8bf3b'
+      clientId: '1508135381',
+      secret: '9622fe6d509adbe1a88c01ddf4afc9cd'
     });
     Accounts.loginServiceConfiguration.insert({
       service: 'qq',
-      clientId: '1103991407',
-      secret: 'ByiRoD39DiQtYrcG'
+      clientId: '1104127289',
+      secret: 'qYLlb5MW8AlxvK3M'
     });
     Accounts.loginServiceConfiguration.insert({
       service: 'wechat',
-      appId: '123490452',
-      secret: 'f69795795d96fe8518a4f0bc14d8bf3b'
+      appId: 'wx599196add0e17def',
+      secret: '783e129bc26650acb5791f19c0e476fc'
     });
     Meteor.publish('allUsers', function() {
       return Meteor.users.find();
