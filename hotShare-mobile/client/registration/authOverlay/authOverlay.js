e83f4577b17127e9414123f60c612a2c7eebe939
timzhang <migrater@msn.cn>
2015-02-14 10:46:59 +0800

    修改webHome footer样式。
    Change-Id: I953d34335919a1a2a29e3d47e4b997ad1eec40fc

--- hotShare-mobile/client/registration/authOverlay/authOverlay.js
+++ hotShare-mobile/client/registration/authOverlay/authOverlay.js
@@ -55,6 +55,7 @@ if (Meteor.isClient) {
   });
   Template.webHome.rendered = function() {
     $('.webHome').css('height', $(window).height());
+    $('.webFooter').css('left', $(window).width()*0.5-105);
   };
 }
 
