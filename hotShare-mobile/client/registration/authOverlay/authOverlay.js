573a3ba8d567f12bad2e5d14fa05a51c88401575
timzhang <migrater@msn.cn>
2015-02-14 15:22:59 +0800

    调整web页面窗口大小时，调整其样式。
    Change-Id: I91cea3878c3a814e6eca408712a904fcc0963401

--- hotShare-mobile/client/registration/authOverlay/authOverlay.js
+++ hotShare-mobile/client/registration/authOverlay/authOverlay.js
@@ -57,5 +57,11 @@ if (Meteor.isClient) {
     $('.webHome').css('height', $(window).height());
     $('.webFooter').css('left', $(window).width()*0.5-105);
   };
+  Meteor.startup(function() {
+    $(window).resize(function() {
+      $('.webHome').css('height', $(window).height());
+      $('.webFooter').css('left', $(window).width()*0.5-105);
+    });
+  });
 }
 
--- hotShare-web/client/webHome.js
+++ hotShare-web/client/webHome.js
@@ -2,3 +2,9 @@ Template.webHome.rendered = function() {
     $('.webHome').css('height', $(window).height());
     $('.webFooter').css('left', $(window).width()*0.5-105);
   };
+Meteor.startup(function() {
+    $(window).resize(function() {
+      $('.webHome').css('height', $(window).height());
+      $('.webFooter').css('left', $(window).width()*0.5-105);
+    });
+  });
