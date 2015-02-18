cf630c198168040aba77f8f2567317bb45790595
Simba Zhang <zzhang@actiontec.com>
2015-02-17 16:25:08 -0800

    Bump versio to 0.6.0 for the changing deployment from ip to domain(app on app.tiegushi.com, shared link on www.tiegushi.com).
    Change-Id: I132aaee14de334c2bb7fa286f37ef8c82da33b98

--- hotShare-mobile/mobile-config.js
+++ hotShare-mobile/mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   id: 'org.hotshare.everywhere',
-  version: '0.5.6',
+  version: '0.6.0',
   name: 'hotShare',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
