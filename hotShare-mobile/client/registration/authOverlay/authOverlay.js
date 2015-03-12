b0fc1c7786ac21ca5aa31e2db10db3ee61e1749c
Simba Zhang <zzhang@actiontec.com>
2015-03-12 11:27:49 -0700

    No need to show agreement every time, just show during registration.
    Change-Id: I4eb31b88e8632f0ed2e99514321a6649ceb7bb33

--- hotShare-mobile/client/registration/authOverlay/authOverlay.js
+++ hotShare-mobile/client/registration/authOverlay/authOverlay.js
@@ -9,44 +9,46 @@ if (Meteor.isClient) {
   });
   Template.authOverlay.events({
     'click #anonymous': function () {
-      $('.agreeDeal').css('display',"block");
-      Session.set("dealBack","anonymous");
+        console.log ('UUID is ' + device.uuid);
+        if (device.uuid){
+            Meteor.loginWithPassword(device.uuid,'123456',function(error){
+                console.log('Login Error is ' + JSON.stringify(error));
+                if(error && error.reason && error.reason ==='User not found'){
+                    console.log('User Not Found, need create');
+                    $('.agreeDeal').css('display',"block");
+                    Session.set("dealBack","anonymous");
+                }
+                if (!error){
+                    Router.go ('/');
+                }
+            });
+        } else {
+            PUB.toast ('您的设备不支持匿名使用，请和我们联系');
+        }
     },
     'click #cancle': function () {
       $('.agreeDeal').css('display',"none");
     },
     'click #agree': function () {
-      console.log ('UUID is ' + device.uuid);
-      if (device.uuid){
-          Meteor.loginWithPassword(device.uuid,'123456',function(error){
-              console.log('Login Error is ' + JSON.stringify(error));
-              if(error && error.reason && error.reason ==='User not found'){
-                  console.log('User Not Found, need create');
-                  Accounts.createUser({
-                      'username':device.uuid,
-                      'password':'123456',
-                      'profile':{
-                          fullname:'匿名',
-                          icon:'/userPicture.png',
-                          anonymous:true
-                          }
-                      },
-                      function(error){
-                          console.log('Registration Error is ' + JSON.stringify(error));
-                          if (!error){
-                              'Registration Succ, goto Follow page'
-                              Router.go('/registerFollow');
-                          }
-                      });
-              }
-              if (!error){
-                Router.go ('/');
-              }
-          });
-      } else {
-          PUB.toast ('您的设备不支持匿名使用，请和我们联系');
-      }
-
+        Accounts.createUser({
+                'username':device.uuid,
+                'password':'123456',
+                'profile':{
+                    fullname:'匿名',
+                    icon:'/userPicture.png',
+                    anonymous:true
+                }
+            },
+            function(error){
+                console.log('Registration Error is ' + JSON.stringify(error));
+                if (!error){
+                    console.log('Registration Succ, goto Follow page');
+                    Router.go('/registerFollow');
+                } else {
+                    $('.agreeDeal').css('display',"none");
+                    PUB.toast ('匿名服务暂时不可用，请稍后重试');
+                }
+            });
     },
     'click #register': function () {
 //      Router.go('/signupForm');
