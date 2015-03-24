6540d909863b8cfee9cc7b5c4a4ba6f0816ff3e3
Tim Zhang <zhzhang@actiontec.com>
2015-03-24 17:35:51 +0800

    Test version 0.8.6
    Change-Id: Ied8e783f480288921503d45a5f510c6a1f2bdfc1

--- hotShareMobile/mobile-config.js
+++ hotShareMobile/mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   id: 'org.hotshare.everywhere',
-  version: '0.8.5',
+  version: '0.8.6',
   name: 'hotShare',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
