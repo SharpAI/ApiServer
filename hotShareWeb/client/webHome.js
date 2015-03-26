5ed273dc8fd1e456459bf25e7bd9895d4567c986
timzhang <migrater@msn.cn>
2015-03-26 16:29:05 +0800

    在web端ios下载增加微信浏览器判断和提示。
    Change-Id: I28749a20c09aecc50650b2e9460bb871f32822ef

--- hotShareWeb/client/webHome.js
+++ hotShareWeb/client/webHome.js
@@ -2,6 +2,15 @@ Template.webHome.rendered = function() {
     $('.webHome').css('height', $(window).height());
     $('.webFooter').css('left', $(window).width()*0.5-105);
   };
+Template.webHome.events({
+    'click .iosBtn':function(){
+      var userAgent = navigator.userAgent.toLowerCase();
+      if(userAgent.indexOf('micromessenger')>=0){
+        document.write('微信内无法下载，请点击“右上角”按钮，选择“在浏览器中打开”即可正常下载');
+        window.location.href = 'itms-apps://itunes.apple.com/app/gu-shi-tie/id957024953';
+      }
+    }
+});
 Meteor.startup(function() {
     $(window).resize(function() {
       $('.webHome').css('height', $(window).height());
