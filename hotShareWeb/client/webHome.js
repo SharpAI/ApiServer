1333fa7b396289473925a67ffac4111dedf3227b
timzhang <migrater@msn.cn>
2015-03-30 12:05:19 +0800

    在微信内访问webHome，实现app Store跳转。
    Change-Id: I4fbd38b74adaa194e89f468c4214b981b74e43c0

--- hotShareWeb/client/webHome.js
+++ hotShareWeb/client/webHome.js
@@ -6,8 +6,8 @@ Template.webHome.events({
     'click .iosBtn':function(){
       var userAgent = navigator.userAgent.toLowerCase();
       if(userAgent.indexOf('micromessenger')>=0){
-        document.write('微信内无法下载，请点击“右上角”按钮，选择“在浏览器中打开”即可正常下载');
-        window.location.href = 'itms-apps://itunes.apple.com/app/gu-shi-tie/id957024953';
+//        document.write('微信内无法下载，请点击“右上角”按钮，选择“在浏览器中打开”即可正常下载');
+        window.location.href = 'http://a.app.qq.com/o/simple.jsp?pkgname=org.hotshare.everywhere';
       }
     }
 });
