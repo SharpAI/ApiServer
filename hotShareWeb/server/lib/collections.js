855b2f464f702ba4db7bf5eeb1b639f1aeff91fe
Tim Zhang <zhzhang@actiontec.com>
2015-04-08 23:22:16 +0800

    同步web端新注册用户添加随机头像代码。
    Change-Id: I496ba20d5e4b94b888aa74da5ce148d287fcdd9e

---
+++ hotShareWeb/server/lib/collections.js
@@ -0,0 +1,13 @@
+Meteor.startup(function(){
+    Accounts.onCreateUser(function(options, user) {
+      var randomI = parseInt(Math.random()*33+1);
+      if (options.profile)
+      {
+        user.profile = options.profile;
+        user.profile.icon = 'http://data.tiegushi.com/anonymousIcon/anonymous_' + randomI + '.png';
+        if(user.profile.name)
+          user.profile.fullname = user.profile.name;
+      }
+      return user;
+    });
+});
