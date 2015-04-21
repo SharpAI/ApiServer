353f57cb6053421144b7a7f391cd3b5a836b1c1a
Simba Zhang <zzhang@actiontec.com>
2015-04-21 11:21:20 -0700

    .wait() will cause long delay on main GUI, it's not good.
    Revert "Split subscribe into related view(wait perfect)."
    
    This reverts commit 370fc42516916b36f0133b5c1847fc8e6507a2f6

--- hotShareMobile/lib/collections.js
+++ hotShareMobile/lib/collections.js
@@ -1019,8 +1019,6 @@ if(Meteor.isClient){
                 Meteor.subscribe("saveddrafts");
                 Meteor.subscribe("topicposts");
                 Meteor.subscribe("topics");
-                
-                // 数据量小保留
                 Meteor.subscribe("follows");
                 Meteor.subscribe("follower");
             },3000);
