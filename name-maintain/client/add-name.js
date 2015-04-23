22b6186255cabfdb076fcdbcdadbcdf697e26e9f
Simba Zhang <zzhang@actiontec.com>
2015-04-23 15:58:31 -0700

    Added statics of hotshare.
    Change-Id: Ie396cc56a23df9fc2824dba6abd423c0a6e5bc26

--- name-maintain/client/add-name.js
+++ name-maintain/client/add-name.js
@@ -1,5 +1,36 @@
 if (Meteor.isClient) {
+  Template.body.rendered = function(){
+    Meteor.call('getStatics',function(error,data){
+      if (error){
+        console.log('got error from call');
+        return;
+      }
+      Session.set('statics_data',data);
+      console.log("Got data " + JSON.stringify(data));
+    });
+  };
   Template.body.helpers({
+    total_posts: function(){
+      return Session.get('statics_data').total_posts;
+    },
+    total_users: function(){
+      return Session.get('statics_data').total_users;
+    },
+    browser_users: function(){
+      return Session.get('statics_data').browser_users;
+    },
+    app_users:function(){
+      return Session.get('statics_data').app_users;
+    },
+    named_users:function(){
+      return Session.get('statics_data').named_users;
+    },
+    total_views:function(){
+      return Session.get('statics_data').total_views;
+    },
+    total_meets:function(){
+      return Session.get('statics_data').total_meets;
+    },
     comments: function () {
       return RefNames.find({}, {sort: {createdAt: -1}});
     },
--- name-maintain/lib/collection.js
+++ name-maintain/lib/collection.js
@@ -1,1 +1,47 @@
 RefNames = new Mongo.Collection("refnames");
+
+if (Meteor.isServer){
+    Posts = new Meteor.Collection('posts');
+    FollowPosts = new Meteor.Collection('followposts');
+    Feeds = new Meteor.Collection('feeds');
+    Drafts = new Meteor.Collection(null);
+    SavedDrafts = new Meteor.Collection('saveddrafts');
+    Follows = new Meteor.Collection('follows');
+    Follower = new Meteor.Collection('follower');
+    Topics = new Meteor.Collection('topics');
+    TopicPosts = new Meteor.Collection('topicposs');
+    Comment = new Meteor.Collection('comment');
+    Viewers = new Meteor.Collection('viewers');
+    RefComments = new Meteor.Collection("refcomments");
+    ReComment = new Meteor.Collection('recomment');
+    Reports = new Meteor.Collection('reports');
+    Messages = new Meteor.Collection('messages');
+    MsgSession = new Meteor.Collection('msgsession');
+    MsgGroup = new Meteor.Collection('msggroup');
+    Meets = new Meteor.Collection('meets');
+    Users = new Meteor.Collection ('users');
+    Meteor.methods({
+            'getStatics':function(){
+                var total_posts = Posts.find().count();
+                var total_users = Users.find().count();
+                var total_meets = Meets.find().count()/2;
+                var browser_users = Users.find({'profile.browser':true}).count();
+                var app_users = Users.find({'profile.browser':{$ne:true}}).count();
+                var named_users = Users.find({'profile.anonymous':{$ne:true}}).count();
+                var total_views = Viewers.find().count();
+                return {
+                    total:1000,
+                    total_posts:total_posts,
+                    total_users:total_users,
+                    browser_users:browser_users,
+                    app_users:app_users,
+                    named_users:named_users,
+                    total_views:total_views,
+                    total_meets:total_meets
+                };
+            }
+        }
+    )
+
+}
+
