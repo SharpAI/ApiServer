3ac6b1569ba7039abc38cad8da75a42af42ee01b
LeonZhang <lzhang@actiontec.com>
2015-05-07 14:19:54 -0700

    add import function

--- src/android/InAppBrowserDialog.java
+++ src/android/InAppBrowserDialog.java
@@ -48,11 +48,7 @@ public class InAppBrowserDialog extends Dialog {
         } else {
             // better to go through the in inAppBrowser
             // because it does a clean up
-            if (this.inAppBrowser.hardwareBack() && this.inAppBrowser.canGoBack()) {
-                this.inAppBrowser.goBack();
-            }  else {
-                this.inAppBrowser.closeDialog();
-            }
+            this.inAppBrowser.closeDialog("");
         }
     }
 }
