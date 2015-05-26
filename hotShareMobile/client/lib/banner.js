b4679f35487ec5168160db1fff27bfec974da15f
Yunhai Zhu <yunhaia@gmail.com>
2015-05-26 11:24:02 -0700

    add connection lost banner to hotshare like wechat, notifiy user when they lost connection
    Change-Id: Id05b5df21e34aeedcea54191ea36534949bcd8cb

---
+++ hotShareMobile/client/lib/banner.js
@@ -0,0 +1,81 @@
+Meteor.startup(function(){
+	Template.connectionBanner.events({
+		'click #connection-try-reconnect': function(event, template){
+			event.preventDefault();
+			Meteor.reconnect();
+		}
+	});
+
+	Template.connectionBanner.helpers({
+		'wasConnected': function(event, template){
+			return Session.equals('MeteorConnection-wasConnected', true);
+		},
+		'isDisconnected': function(event, template){
+			return Session.equals('MeteorConnection-isConnected', false);
+		},
+		'retryTimeSeconds': function(event, template){
+			return Session.get('MeteorConnection-retryTimeSeconds');
+		},
+		'failedReason': function(event, template){
+			return Session.get('MeteorConnection-failedReason');
+		},
+		'connectionLostText': function(event, template){
+			var defaultText = "当前网络不可用，请检查你的网络设置!";
+			if(Meteor.settings && Meteor.settings.public && Meteor.settings.public.connectionBanner && Meteor.settings.public.connectionBanner.connectionLostText)
+				return Meteor.settings.public.connectionBanner.connectionLostText;
+			else
+				return defaultText;
+		},
+		'tryReconnectText': function(event, template){
+			var defaultText = "点击尝试重新连接";
+			if(Meteor.settings && Meteor.settings.public && Meteor.settings.public.connectionBanner && Meteor.settings.public.connectionBanner.tryReconnectText)
+				return Meteor.settings.public.connectionBanner.tryReconnectText;
+			else
+				return defaultText;
+		},
+		'reconnectBeforeCountdownText': function(event, template){
+			var defaultText = "正在尝试自动重连";
+			if(Meteor.settings && Meteor.settings.public && Meteor.settings.public.connectionBanner && Meteor.settings.public.connectionBanner.reconnectBeforeCountdownText)
+				return Meteor.settings.public.connectionBanner.reconnectBeforeCountdownText;
+			else
+				return defaultText;
+		},
+		'reconnectAfterCountdownText': function(event, template){
+			var defaultText = "秒.";
+			if(Meteor.settings && Meteor.settings.public && Meteor.settings.public.connectionBanner && Meteor.settings.public.connectionBanner.reconnectAfterCountdownText)
+				return Meteor.settings.public.connectionBanner.reconnectAfterCountdownText;
+			else
+				return defaultText;
+		}
+	});
+
+	Session.setDefault('MeteorConnection-isConnected', true);
+	Session.setDefault('MeteorConnection-wasConnected', false);
+	Session.setDefault('MeteorConnection-retryTimeSeconds', 0);
+	Session.setDefault('MeteorConnection-failedReason', null);
+	var connectionRetryUpdateInterval;
+
+	Deps.autorun(function(){
+		var isConnected = Meteor.status().connected;
+		if(isConnected){
+			Session.set('MeteorConnection-wasConnected', true);
+			Meteor.clearInterval(connectionRetryUpdateInterval);
+			connectionRetryUpdateInterval = undefined;
+			Session.set('MeteorConnection-retryTimeSeconds', 0);
+			Session.set('MeteorConnection-failedReason', null);
+		}else{
+			if(Session.equals('MeteorConnection-wasConnected', true)){
+				if(!connectionRetryUpdateInterval)
+					connectionRetryUpdateInterval = Meteor.setInterval(function(){
+						var retryIn = Math.round((Meteor.status().retryTime - (new Date()).getTime())/1000);
+						if(isNaN(retryIn))
+							retryIn = 0;
+						Session.set('MeteorConnection-retryTimeSeconds', retryIn);
+						Session.set('MeteorConnection-failedReason', Meteor.status().reason);
+					},500);
+			}
+		}
+		Session.set('MeteorConnection-isConnected', isConnected);
+	});
+});
+	
