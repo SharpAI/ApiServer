26439bbd54110fafd7dceb4d8a4bb7dbcd270e07
Simba Zhang <zzhang@actiontec.com>
2015-06-05 14:29:09 -0700

    Removed debug log, added comment.
    Change-Id: I7f5ced60325eda02b2688604c3ee061aff04b335

--- pullServer/server/puller.js
+++ pullServer/server/puller.js
@@ -13,29 +13,19 @@ if (Meteor.isServer){
           if (url.startsWith("http")) {
             requestUrl = url;
           } else {
+            //If passed the post ID, add prefix. Post ID will be easier if changed the domain name of CDN,
+            //just need upgrade this small server instead of the whole website server.
             requestUrl = 'http://cdn.tiegushi.com/posts/' + url;
           }
 
-          console.log("processing pullFromServer " + requestUrl);
           Meteor.http.get(requestUrl, {
+            // The Accept-Encoding gzip is the most important. Without this, the response will have no inject-data.
             headers: {
               'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
               'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.81 Safari/537.36',
               'Accept-Encoding': 'gzip, deflate, sdch'
             }
-          }, function (error, result) {
-            if(error) {
-              console.log('http get FAILED!');
-            } else {
-              console.log('http get SUCCES');
-              if (result.statusCode === 200) {
-                console.log('Status code = 200!');
-                //console.log(result.content);
-                console.log(result);
-              }
-            }
           });
-          //s.get(requestUrl, function(retval) {console.log('Link('+ requestUrl+') got ' + retval);});
         }
       } catch(error){}}
     );
@@ -44,6 +34,4 @@ if (Meteor.isServer){
   Meteor.methods({
     "pullFromServer": pullFromServer
   });
-
-  Meteor.call("pullFromServer","http://cdn.tiegushi.com/posts/CY9RQnawWnXHDBxLM");
 }
