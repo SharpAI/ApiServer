89685fa3cd9ca92479c3603be4d3e1484e5ab6b4
LeonZhang <lzhang@actiontec.com>
2015-06-12 16:23:37 -0700

    change to utf8

--- src/android/Library/Android-Ultra-Photo-Selector/src/com/photoselector/ui/PhotoSelectorAdapter.java
+++ src/android/Library/Android-Ultra-Photo-Selector/src/com/photoselector/ui/PhotoSelectorAdapter.java
@@ -45,7 +45,7 @@ public class PhotoSelectorAdapter extends MBaseAdapter<PhotoModel> {
 		this.cameraListener = cameraListener;
 	}
 
-	/** ÉèÖÃÃ¿Ò»¸öItemµÄ¿í¸ß */
+	/** è®¾ç½®æ¯ä¸€ä¸ªItemçš„å®½é«˜ */
 	public void setItemWidth(int screenWidth) {
 		int horizentalSpace = context.getResources().getDimensionPixelSize(R.dimen.sticky_item_horizontalSpacing);
 		this.itemWidth = (screenWidth - (horizentalSpace * (horizentalNum - 1))) / horizentalNum;
