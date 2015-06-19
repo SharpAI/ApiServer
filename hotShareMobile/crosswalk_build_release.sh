27053a7a55a7972f9b87baa3a46dceb110e12dba
Simba Zhang <zzhang@actiontec.com>
2015-06-18 18:27:44 -0700

    Added crosswalk_build_release script.
    Change-Id: I89e70ab393179472e4507320b64be7128b625322

---
+++ hotShareMobile/crosswalk_build_release.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd .meteor/local/cordova-build/
+cordova platform update android
+cordova platform remove ios 
+cordova plugin rm com.file-transfer.baidu.bcs
+rm -rf  local-plugins/com.file-transfer.baidu.bcs
+rm -rf  plugins/com.file-transfer.baidu.bcs
+cordova plugin add https://github.com/solderzzc/cordova-plugin-whitelist.git#r1.1.1
+cordova plugin add https://github.com/MobileChromeApps/cordova-plugin-crosswalk-webview.git#1.2.0
+cordova plugin add https://github.com/solderzzc/cordova-plugin-file-transfer.git#r0.5.1
+cp ../../../../cordova-build-overide/config.xml ./
+cp ../../../../cordova-build-overide/platforms/android/assets/www/index.html platforms/android/assets/www/index.html
+cordova compile android --release
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore keystore ".meteor/local/cordova-build/platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk" 'wifi whiteboard'
+mv .meteor/local/cordova-build/platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk ./hotShareRelease.apk
