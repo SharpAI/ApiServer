6bfa3ad379d5c1d7a052e345240804f112f851ad
Simba Zhang <zzhang@actiontec.com>
2015-06-18 22:43:22 -0700

    Do not copy file due to the maintain effort.
    Change-Id: Ibc37a97654606cf972832a537ef4a33c47ce3a6e

--- hotShareMobile/crosswalk_build_release.sh
+++ hotShareMobile/crosswalk_build_release.sh
@@ -10,8 +10,9 @@ cordova plugin rm cordova-plugin-whitelist
 cordova plugin add https://github.com/solderzzc/cordova-plugin-whitelist.git#r1.1.1
 cordova plugin add https://github.com/MobileChromeApps/cordova-plugin-crosswalk-webview.git#1.2.0
 cordova plugin add https://github.com/solderzzc/cordova-plugin-file-transfer.git#r0.5.2
-cp ../../../../cordova-build-overide/config.xml ./
-cp ../../../../cordova-build-overide/platforms/android/assets/www/index.html platforms/android/assets/www/index.html
+printf '%s\n' /content/a '  <allow-navigation href="*" subdomain="true" />' . w q | ex -s config.xml 
+printf '%s\n' /content/a '  <access origin="*" subdomains="true"/>' . w q | ex -s config.xml 
+printf "%s\n" /head/a "  <meta http-equiv=\"Content-Security-Policy\" content=\"default-src * data: gap: https://ssl.gstatic.com; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval' http://int.dpool.sina.com.cn\"; >" . w q | ex -s platforms/android/assets/www/index.html
 cordova compile android --release
 cd ../../../
 cp .meteor/local/cordova-build/platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk android-armv7-release-unsigned.apk
