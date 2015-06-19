b6967651223b566775c5d6768cd7a82e31e15a32
leonzhang1109 <leonzhang1109@hotmail.com>
2015-06-18 18:07:02 -0700

    Update WeChat.java

--- src/android/WeChat.java
+++ src/android/WeChat.java
@@ -74,7 +74,7 @@ public class WeChat extends CordovaPlugin {
 
     @Override
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-        String appId = webView.getPreferences().getString(WECHAT_APPID_KEY,"wxd930ea5d5a258f4f");
+        String appId = webView.getPreferences().getString(WECHAT_APPID_KEY,"wxcfcf19c225a36351");
         api = WXAPIFactory.createWXAPI(webView.getContext(), appId, true);
         api.registerApp(appId);
     }
