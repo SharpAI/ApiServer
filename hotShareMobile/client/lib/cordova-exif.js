88b9045a7fb7868d7d8498a2adb36f1245f7ba82
Simba Zhang <zzhang@actiontec.com>
2015-06-28 22:05:41 -0700

    Fixed the bug when image can't be analysed then importing progress hang.
    Change-Id: I34277388130545f5e14862eb9c52e1280a1b67b2

--- hotShareMobile/client/lib/cordova-exif.js
+++ hotShareMobile/client/lib/cordova-exif.js
@@ -1,28 +1,34 @@
 
 get_image_size_from_URI = function(imageURI,callback) {
-// This function is called once an imageURI is rerturned from PhoneGap's camera or gallery function
+	// This function is called once an imageURI is rerturned from PhoneGap's camera or gallery function
 	window.resolveLocalFileSystemURL(imageURI, function(fileEntry) {
 		fileEntry.file(function(fileObject){
-// Create a reader to read the file
+			// Create a reader to read the file
 			var reader = new FileReader();
 
-// Create a function to process the file once it's read
+			// Create a function to process the file once it's read
 			reader.onloadend = function(evt) {
-// Create an image element that we will load the data into
+				console.log('Create an image element that we will load the data into ');
 				var image = new Image();
+				image.onerror = function(){
+					image = null;
+					if (callback){
+						callback(0,0)
+					}
+				};
 				image.onload = function(evt) {
-// The image has been loaded and the data is ready
+					// The image has been loaded and the data is ready
 					var image_width = this.width;
 					var image_height = this.height;
 					console.log("IMAGE HEIGHT: " + image_height);
 					console.log("IMAGE WIDTH: " + image_width);
-// We don't need the image element anymore. Get rid of it.
-					image = null
+					// We don't need the image element anymore. Get rid of it.
+					image = null;
 					if (callback){
 						callback(image_width,image_height)
 					}
 				};
-// Load the read data into the image source. It's base64 data
+				// Load the read data into the image source. It's base64 data
 				image.src = evt.target.result
 			};
 			reader.onabort = function(){
@@ -37,7 +43,7 @@ get_image_size_from_URI = function(imageURI,callback) {
 					callback(0,0)
 				}
 			};
-// Read from disk the data as base64
+			// Read from disk the data as base64
 			reader.readAsDataURL(fileObject)
 		}, function(){
 			console.log("There was an error reading or processing this file.");
@@ -46,4 +52,4 @@ get_image_size_from_URI = function(imageURI,callback) {
 			}
 		})
 	})
-}
+};
