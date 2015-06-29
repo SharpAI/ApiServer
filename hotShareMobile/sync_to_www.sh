b997ed21282e336ad63798d8c4d76574b05c0707
Simba Zhang <zzhang@actiontec.com>
2015-06-29 13:02:30 -0700

    Modified sync_to_www.sh to create directory first.
    Change-Id: I544e4abf092959d066a83a14fd1eac1419ac97a2

--- hotShareMobile/sync_to_www.sh
+++ hotShareMobile/sync_to_www.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo 'Applying changes from meteor build directory'
 cp -rf .meteor/local/build/programs/web.cordova/app/* .meteor/local/cordova-build/www/application/
+mkdir -p .meteor/local/cordova-build/platforms/ios/www/application/
 cp -rf .meteor/local/build/programs/web.cordova/app/* .meteor/local/cordova-build/platforms/ios/www/application/
+mkdir -p .meteor/local/cordova-build/platforms/android/assets/www/application/
 cp -rf .meteor/local/build/programs/web.cordova/app/* .meteor/local/cordova-build/platforms/android/assets/www/application/
 
