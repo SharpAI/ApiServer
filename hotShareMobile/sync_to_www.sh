efc2244557090173317f291df1d3b7e4fe9757d5
Simba Zhang <zzhang@actiontec.com>
2015-06-29 11:53:35 -0700

    Add script to sync the code built from meteor to mobile project directory.
    Change-Id: I731ac867c34867ab7d8e5fc2dfc7fff882ca4ac6

---
+++ hotShareMobile/sync_to_www.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo 'Applying changes from meteor build directory'
+cp -rf .meteor/local/build/programs/web.cordova/app/* .meteor/local/cordova-build/www/application/
+cp -rf .meteor/local/build/programs/web.cordova/app/* .meteor/local/cordova-build/platforms/ios/www/application/
+cp -rf .meteor/local/build/programs/web.cordova/app/* .meteor/local/cordova-build/platforms/android/assets/www/application/
+
