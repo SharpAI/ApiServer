37e40dfe66df5638cfc9248fe6f8c474c8de47ac
Simba Zhang <zzhang@actiontec.com>
2015-06-30 21:26:13 -0700

    Bump version to 0.9.14.
    Change-Id: I845966f4b36f6ddc39c85217f3740d3a9b6173f2

--- hotShareMobile/mobile-config.js
+++ hotShareMobile/mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   id: 'org.hotshare.everywhere',
-  version: '0.9.13',
+  version: '0.9.14',
   name: 'hotShare',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
