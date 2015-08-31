ee8ae9f9dbc22bddc09ff7fd31344420e0738dc4
Leon Zhang <lzhang@actiontec.com>
2015-08-31 11:07:01 -0700

    Revert "解决动态中有朋友读新的帖子后不自动更新的问题"
    This reverts commit 3772fe0e623d044fad73e250d163aeb2a4474aef

--- hotShareWeb/lib/2_collections.js
+++ hotShareWeb/lib/2_collections.js
@@ -259,8 +259,6 @@ if(Meteor.isServer){
                         }
                     }
                 });
-                var currentlimit=Session.get('momentsitemsLimit');
-                Session.set('momentsitemsLimit',1+currentlimit);
             }
         });
     }
