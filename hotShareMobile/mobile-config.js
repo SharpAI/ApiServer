30620126aaefc1afdc7d8efcfd565f98afb04312
LeonZhang <lzhang@actiontec.com>
2015-09-11 16:23:05 -0700

    remove cordova-plugin-media and Bump version to 0.9.59.
    Change-Id: I4ec6e19b23869bf2bb92160ab062749b54a5d215

--- hotShareMobile/mobile-config.js
+++ hotShareMobile/mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   id: 'org.hotshare.everywhere',
-  version: '0.9.58',
+  version: '0.9.59',
   name: 'hotShare',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
