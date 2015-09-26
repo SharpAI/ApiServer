17f666d1b2ff05a7aa5f690ab7197feb6515c9fc
Simba Zhang <zzhang@actiontec.com>
2015-09-25 18:54:08 -0700

    rm cache folder.
    Change-Id: Ie8e3acc4946d7f7e532f9d19ae559bab5f65e084

--- hotShareMobile/build.sh
+++ hotShareMobile/build.sh
@@ -2,4 +2,6 @@
 rm -rf ~/build-hot-share
 rm -rf .meteor/local/cordova-build
 rm -rf .meteor/local/build
+rm -rf .meteor/local/bundler-cache
+rm -rf .meteor/local/plugin-cache
 meteor build ~/build-hot-share --server=http://host1.tiegushi.com
