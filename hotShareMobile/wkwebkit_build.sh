faa68b17478e9ff72c9794a3af507787ce6d1623
Simba Zhang <zzhang@actiontec.com>
2015-09-30 20:29:01 -0700

    Added back the hack method since it's the build error.
    Change-Id: Ic96242004ab56cd273cda43beee94ee4bf8b7e00

--- hotShareMobile/wkwebkit_build.sh
+++ hotShareMobile/wkwebkit_build.sh
@@ -3,5 +3,6 @@ meteor add cordova:com.meteor.cordova-update@file://../cordova-plugins/com.meteo
 meteor add practicalmeteor:wkwebview
 
 sh ./build.sh
+mv .meteor/local/cordova-build/platforms/ios/www/application/app/* .meteor/local/cordova-build/platforms/ios/www/application/
 
-#open -a xcode .meteor/local/cordova-build/platforms/ios/hotShare.xcodeproj/
+open -a xcode .meteor/local/cordova-build/platforms/ios/hotShare.xcodeproj/
