cededd50bc415ce19f693257516cb278abb18792
LeonZhang <lzhang@actiontec.com>
2015-10-23 19:55:07 -0700

    Bump version to 1.0.33.
    Change-Id: I2538d4affc3bc40085e43890c3f16db2ecfe3c30

--- hotShareMobile/mobile-config.js
+++ hotShareMobile/mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   id: 'org.hotshare.everywhere',
-  version: '1.0.32',
+  version: '1.0.33',
   name: 'hotShare',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
