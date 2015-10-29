0e63ccc24940d9411c31bc299381411756d57ae0
LeonZhang <lzhang@actiontec.com>
2015-10-28 17:56:39 -0700

    Bump version to 1.0.37.
    Change-Id: I201b93dd6e2083b05d2c4661f91400583857d1e2

--- hotShareMobile/lib/6_version.js
+++ hotShareMobile/lib/6_version.js
@@ -1,2 +1,2 @@
 
-version_of_build = '1.0.35';
+version_of_build = '1.0.37';
--- hotShareMobile/mobile-config.js
+++ hotShareMobile/mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   id: 'org.hotshare.everywhere',
-  version: '1.0.35',
+  version: '1.0.37',
   name: 'hotShare',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
