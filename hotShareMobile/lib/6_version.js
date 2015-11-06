f8c40d368d4d4b565efc2357e2668ab43c19613a
LeonZhang <lzhang@actiontec.com>
2015-11-05 20:27:32 -0800

    Bump version to 1.0.44.

--- hotShareMobile/lib/6_version.js
+++ hotShareMobile/lib/6_version.js
@@ -1,2 +1,2 @@
 
-version_of_build = '1.0.43';
+version_of_build = '1.0.44';
--- hotShareMobile/mobile-config.js
+++ hotShareMobile/mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   id: 'org.hotshare.everywhere',
-  version: '1.0.43',
+  version: '1.0.44',
   name: 'hotShare',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
