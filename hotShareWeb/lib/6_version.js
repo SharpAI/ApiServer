cf854f5eb84706c4cae1ff6bb547baf641c4318f
Tim Zhang <zhzhang@actiontec.com>
2015-12-03 10:42:58 +0800

    Bump version to 1.0.56.
    Change-Id: Ib6f115ee6badf727dcd5675dacbab5a720de5888

--- hotShareWeb/lib/6_version.js
+++ hotShareWeb/lib/6_version.js
@@ -1,2 +1,2 @@
 
-version_of_build = '1.0.55';
+version_of_build = '1.0.56';
--- hotShareWeb/mobile-config.js
+++ hotShareWeb/mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   id: 'org.hotshare.everywhere',
-  version: '1.0.55',
+  version: '1.0.56',
   name: 'hotShare-web',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
