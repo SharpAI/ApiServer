82c440fc0d676b40ade5ae8c7a6a4b3740090633
Tim Zhang <zhzhang@actiontec.com>
2016-03-12 17:43:55 +0800

    修复 push token 不自动更新。
    Change-Id: Icd0a1cd5774994184d78489b4dff460a23ca901a

---
+++ hotShareMobile/client/0_ios_push_init.js
@@ -0,0 +1,48 @@
+if (Meteor.isClient) {
+  console.log('on isCordova');
+  Meteor.startup(function() {
+    var onDeviceReady;
+    console.log('on startup');
+    if (device.platform === 'iOS') {
+      console.log('on IOS');
+      this.onNotificationAPN = function(event) {
+        console.log('Got message');
+        if (event.foreground === '0') {
+          console.log('Push notification when background');
+          window.refreshMainDataSource();
+          return;
+        }
+        if (event.alert) {
+          PUB.toast(event.alert);
+          window.plugin.notification.local.cancelAll();
+          window.plugin.notification.local.add({
+            message: event.alert
+          });
+          return window.refreshMainDataSource();
+        }
+      };
+      onDeviceReady2 = function() {
+        var registerInterval1 = window.setInterval( function(){
+        console.log('on onDeviceReady2');
+        window.plugins.pushNotification.register(function(result) {
+          console.log('Got registrationID ' + result);
+          Session.set('registrationID', result);
+          Session.set('registrationType', 'iOS');
+          window.clearInterval(registerInterval1);
+          return window.updatePushNotificationToken('iOS', result);
+        }, function(error) {
+          return console.log('No Push Notification support in this build error = ' + error);
+        }, {
+          "badge": "true",
+          "sound": "true",
+          "alert": "true",
+          "ecb": "window.onNotificationAPN"
+        });
+         },20000 );
+      };
+      Deps.autorun(function(){
+        document.addEventListener("deviceready", onDeviceReady2, false);
+      });
+    }
+  });
+}
