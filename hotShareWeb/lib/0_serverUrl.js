be3f216328cd45d500a89f022227d5378250c960
Peng Zhu <manlinzhu@gmail.com>
2016-04-19 14:18:26 +0800

    fixed issue: process is not defined, Cannot read property 'ServiceConfiguration' of undefined

--- hotShareWeb/lib/0_serverUrl.js
+++ hotShareWeb/lib/0_serverUrl.js
@@ -1,1 +1,1 @@
-server_domain_name = process.env.SERVER_DOMAIN_NAME || "cdn.tiegushi.com";
+server_domain_name = (typeof process !== "undefined" && process !== null ? process.env.SERVER_DOMAIN_NAME : void 0) || "cdn.tiegushi.com";
