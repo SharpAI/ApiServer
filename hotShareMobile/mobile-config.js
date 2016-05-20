f70a8ea406570bd8020f910024c8d25f15b5c538
Zhangzhiyue <migrater@msn.cn>
2016-05-20 10:51:45 +0800

    Bump version to 1.1.15.

--- hotShareMobile/mobile-config.js
+++ hotShareMobile/mobile-config.js
@@ -9,7 +9,7 @@
 // });
 App.info({
   id: 'org.hotshare.everywhere',
-  version: '1.1.13',
+  version: '1.1.15',
   name: 'hotShare',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
--- hotShareWeb/lib/6_version.js
+++ hotShareWeb/lib/6_version.js
@@ -1,1 +1,1 @@
-version_of_build = '1.1.14';
+version_of_build = '1.1.15';
