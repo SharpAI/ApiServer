abaa627773f5c47a92045e28aee7b6924eeed1c3
Zhangzhiyue <migrater@msn.cn>
2016-05-27 14:09:45 +0800

    Bump version to 1.1.20.

--- hotShareMobile/lib/6_version.js
+++ hotShareMobile/lib/6_version.js
@@ -1,1 +1,1 @@
-version_of_build = '1.1.19';
+version_of_build = '1.1.20';
--- hotShareWeb/mobile-config.js
+++ hotShareWeb/mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   id: 'org.hotshare.everywhere',
-  version: '1.1.15',
+  version: '1.1.20',
   name: 'hotShare-web',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
