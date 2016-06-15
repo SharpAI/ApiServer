dd4f3b8ec40ba9c38be5372bddab1449e703cf45
sraita <sraita@139.com>
2016-06-15 18:34:36 +0800

    Bump version to 1.1.31

--- hotShareMobile/lib/6_version.js
+++ hotShareMobile/lib/6_version.js
@@ -1,1 +1,1 @@
-version_of_build = '1.1.30';
+version_of_build = '1.1.31';
--- hotShareWeb/mobile-config.js
+++ hotShareWeb/mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   id: 'org.hotshare.everywhere',
-  version: '1.1.30',
+  version: '1.1.31',
   name: 'hotShare-web',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
