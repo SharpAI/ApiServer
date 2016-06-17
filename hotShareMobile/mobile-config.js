601d7602c77d44fe436a725b6c80725dab19366c
sraita <sraita@139.com>
2016-06-17 18:55:10 +0800

    Bump version to 1.1.34

--- hotShareMobile/mobile-config.js
+++ hotShareMobile/mobile-config.js
@@ -9,7 +9,7 @@
 // });
 App.info({
   id: 'org.hotshare.everywhere',
-  version: '1.1.32',
+  version: '1.1.34',
   name: 'hotShare',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
