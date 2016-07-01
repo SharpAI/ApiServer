d5521d324ec89c3f54c0d6afaf00616875a1736c
Simba Zhang <zzhang@actiontec.com>
2016-06-30 17:43:17 -0700

    Previous commit of insert to mongodb is not usable.
    Change-Id: Ieb145d821291210a95523d9819a4016740857dd4

--- urlAnalyser/main.js
+++ urlAnalyser/main.js
@@ -3,6 +3,8 @@ var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
 var path = require('path');
+var mongoid = require('mongoid-js');
+
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,20 +13,24 @@ app.use(bodyParser.json());
 var port = process.env.PORT || 8080;        // set our port
 var MongoClient = require('mongodb').MongoClient;
 var DB_CONN_STR = 'mongodb://127.0.0.1:3001/meteor';
+var posts = null;
 
-var insert_data = function(id, url, data, cb) {
-  if (!id || !data || !url) {
-    console.log('Error: null of id or data');
-  }
-
-  MongoClient.connect(DB_CONN_STR, function(err, db) {
+MongoClient.connect(DB_CONN_STR, function(err, db) {
     if (err) {
-        console.log('Error:'+ err);
+        console.log('Error:' + err);
         return;
     }
+    posts = db.collection('posts');
+});
+var insert_data = function(id, url, data, cb) {
+    if (!id || !data || !url || !posts) {
+      console.log('Error: null of id or data');
+      if(cb){
+          cb('error',null)
+      }
+      return;
+    }
 
-    collection = db.collection('posts');
-    
     if(data.resortedArticle.length > 0){
       for(var i=0;i<data.resortedArticle.length;i++){
         if(data.resortedArticle[i].type === 'image')
@@ -38,7 +44,8 @@ var insert_data = function(id, url, data, cb) {
     }
     
     var data_insert = [{
-      '_id': id,
+      '_id':id+'_'+mongoid(),
+      'ownerId': id,
       'pub': data.resortedArticle,
       'title': data.title,
       'browse': 0,
@@ -51,19 +58,23 @@ var insert_data = function(id, url, data, cb) {
       'mainImageStyle': [],
       'mainText': [],
       'fromUrl': url,
+      'status':'importing',
       'publish': true
       }];
 
-    collection.insert(data_insert, function(err, result) { 
-    //collection.find(whereStr).toArray(function(err, result) {
-      if(err) {
+    posts.insert(data_insert, function(err, result) {
+      if(err || !result.insertedCount || !result.insertedIds || !result.insertedIds[0]) {
         console.log('Error:'+ err);
-        return;
+        if(cb){
+          cb(err,null)
+        }
+        return null;
       }
-      console.log(result);
-      db.close();
+      console.log(result.insertedIds[0]);
+      if(cb){
+          cb(null,result.insertedIds[0])
+      }
     });
-  });
 }
 
 // ROUTES FOR OUR API
@@ -77,14 +88,9 @@ router.get('/', function(req, res) {
 
 // more routes for our API will happen here
 router.route('/:_id/:url')
-  // get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)
     .get(function(req, res) {
       console.log('_id=' + req.params._id + ' url=' + req.params.url);
-      /*if (err)
-       res.send(err);*/
-      var nightmare = Nightmare({ show: true , openDevTools: true/*, webPreferences: {
-       nodeIntegration: true
-       }*/})
+      var nightmare = Nightmare({ show: true , openDevTools: true});
       nightmare
           .goto(req.params.url)
           .inject('js','bundle.js')
@@ -98,24 +104,22 @@ router.route('/:_id/:url')
             if(!req.state){
               req.state = true
 
-
-              insert_data(req.params._id, req.params.url, result, function(err){
+              insert_data(req.params._id, req.params.url, result, function(err,postId){
                 if (err) {
                   console.log('Error: insert_data failed');
+                  res.json({status:'failed'});
+                  return;
                 }
-              });
+                console.log('Post id is: '+postId);
 
-              res.json({status:'ok',json:result});
+                res.json({status:'succ',json:'http://cdn.tiegushi.com/posts/'+postId});
+              });
             }
           })
           .catch(function (error) {
-            res.json({status:'failed',json:result});
+            res.json({status:'failed'});
             console.error('Search failed:', error);
           })
-      // you can do whatever you want here..
-      /*ipcMain.once('analyse-done', function(event, arg) {
-       console.log(arg);
-       });*/
     });
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
