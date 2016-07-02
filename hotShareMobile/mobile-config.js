c3a660896bfd2180affc2b6f5430de0088ccdb38
Zhangzhiyue <migrater@msn.cn>
2016-07-02 17:32:40 +0800

    Bump version to 1.2.7.

--- hotShareMobile/mobile-config.js
+++ hotShareMobile/mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   id: 'org.hotshare.everywhere',
-  version: '1.2.6',
+  version: '1.2.7',
   name: 'hotShare',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
--- hotShareWeb/lib/6_version.js
+++ hotShareWeb/lib/6_version.js
@@ -1,1 +1,1 @@
-version_of_build = '1.2.6';
+version_of_build = '1.2.7';
