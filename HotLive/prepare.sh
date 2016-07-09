1040483f1339dfe15211a666c494444d12416bd6
Simba Zhang <zzhang@actiontec.com>
2016-07-09 11:08:03 -0700

    Try to use ios target to 8.0, if any issue, need modify hooks/iosrtc-swift-support.js. Add permission into config.xml for Android. Changed prepare.sh to use ios@4.0.0 with iosrtc plugin @ 3.0.1
    Change-Id: I71aeae405725f1acb19aa210b96df6c8ed536b72

--- HotLive/prepare.sh
+++ HotLive/prepare.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm -rf plugins/cordova-*
 npm install xcode
 cordova platforms remove ios
 cordova platforms remove android
@@ -7,8 +8,9 @@ cordova plugins add cordova-plugin-chrome-apps-power@1.0.4
 cordova plugins add cordova-plugin-crosswalk-webview@1.5.0
 cordova plugins add cordova-plugin-device
 cordova plugins add cordova-plugin-file@4.1.1
-cordova plugins add cordova-plugin-iosrtc@2.2.2
+cordova plugins add cordova-plugin-iosrtc@3.0.1
 cordova plugins add cordova-plugin-whitelist@1.2.1
 
 cordova platforms add android@5.1.0
-cordova platforms add ios@3.9.2
+#cordova platforms add ios@3.9.2
+cordova platforms add ios@4.0.0
