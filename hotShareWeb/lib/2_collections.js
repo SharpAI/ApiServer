351dbf264b72a08ccf92525429508a314f88dbef
Simba Zhang <zzhang@actiontec.com>
2016-07-14 12:18:03 -0700

    Don't push all information of posts to the client.
    Change-Id: I1f97425d712f5fe90b9c92adb3a1390316da58e0

--- hotShareWeb/lib/2_collections.js
+++ hotShareWeb/lib/2_collections.js
@@ -1481,8 +1481,7 @@ if(Meteor.isServer){
           return this.ready();
       }
       else{
-          return this.ready();
-          //return Posts.find({owner: this.userId},{sort: {createdAt: -1},limit:limit});
+          return Posts.find({owner: this.userId},{sort: {createdAt: -1},limit:limit,fields:{mainImage:1,title:1,addontitle:1,publish:1,owner:1,createdAt:1}});
       }
   });
   Meteor.publish("savedDraftsWithLimit", function(limit) {
@@ -2576,14 +2575,14 @@ if(Meteor.isClient){
                       Session.set('followeesCollection', 'loaded');
                   }
               });
-              /*Meteor.subscribe('postsWithLimit', Session.get('mypostsitemsLimit'), {
+              Meteor.subscribe('postsWithLimit', Session.get('mypostsitemsLimit'), {
                   onReady: function(){
                       console.log('myPostsCollection loaded');
                       Meteor.setTimeout(function(){
                         Session.set('myPostsCollection','loaded');
                       },500);
                   }
-              });*/
+              });
 
               Meteor.subscribe('readerpopularposts', {
                   onReady: function(){
