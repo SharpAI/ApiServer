deb5a4d29e75cf1f683a8aee387c18510ee35ee1
sraita <sraita@139.com>
2016-08-04 19:31:54 +0800

    Bump Version to 1.2.26 .

--- hotShareMobile/mobile-config.js
+++ hotShareMobile/mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   id: 'org.hotshare.everywhere',
-  version: '1.2.25',
+  version: '1.2.26',
   name: 'hotShare',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
--- hotShareWeb/lib/6_version.js
+++ hotShareWeb/lib/6_version.js
@@ -1,1 +1,1 @@
-version_of_build = '1.2.25';
+version_of_build = '1.2.26';
