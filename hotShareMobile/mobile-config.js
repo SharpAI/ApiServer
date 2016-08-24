ea75a7f5c17506d7d461752c0be74e5e7abfe555
bergxu <bexu@actiontec.com>
2016-08-24 09:55:55 +0800

    Bump version to 1.3.8
    Change-Id: Ie64ef1efe7c6f032cf2b1ffc89d5bd6fc509d732

--- hotShareMobile/mobile-config.js
+++ hotShareMobile/mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   id: 'org.hotshare.everywhere',
-  version: '1.3.7',
+  version: '1.3.8',
   name: 'hotShare',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
