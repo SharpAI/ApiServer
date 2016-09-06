c11b999f65422601f8998319ba28474b487f68da
Simba Zhang <solderzzc@gmail.com>
2016-09-06 12:05:34 -0700

    Added user password auth. Added remove self command. Added get all command. /start help.
    Change-Id: I6fbcd5bd848adb8c464a5725d2b3f9a285c0d4d6

--- PostReporter/main.js
+++ PostReporter/main.js
@@ -13,18 +13,14 @@ var client  = mqtt.connect('ws://rpcserver.raidcdn.com:80');
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var url = 'mongodb://hotShareAdmin:aei_19056@host1.tiegushi.com:27017/hotShare';
+var userDBURL = 'mongodb://postevent:akak(*&654@ds019786.mlab.com:19786/postevent'
 //var url = 'mongodb://localhost:27017/localdb';
 var db = null
+var userDB = null
 var TelegramBot = require('node-telegram-bot-api');
 var token = '245939457:AAE9qEYvnNv1A5hOfkkRwwxBnEU0qE6RHyE';
 // Setup polling way
 var bot = new TelegramBot(token, {polling: true});
-var Datastore = require('nedb')
-    , db = new Datastore({ filename: 'idsaver' });
-db.loadDatabase(function (err) {    // Callback is optional
-    // Now commands will be executed
-});
-
 
 
 process.addListener('uncaughtException', function (err) {
@@ -42,6 +38,132 @@ MongoClient.connect(url, function(err, tdb) {
     assert.equal(null, err);
     db=tdb
 });
+MongoClient.connect(userDBURL, function(err, tdb) {
+    assert.equal(null, err);
+    userDB=tdb
+    console.log('Connected to userDB')
+    update_send_list()
+});
+
+var telegram_password = '98216cka';
+var send_list = [];
+
+function add_auth_user_info_to_db (username,chat_id,callback){
+    userDB.collection('auth_users').insert({username:username,chat_id:chat_id},callback);
+}
+function update_auth_user_info_to_db (username,chat_id,callback){
+    userDB.collection('auth_users').update({username:username},{chat_id:chat_id},{upsert:true},callback);
+}
+function remove_auth_user_info_to_db (username,chat_id,callback){
+    userDB.collection('auth_users').remove({username:username},callback);
+}
+function check_if_legit_user(username,callback){
+    userDB.collection('auth_users').findOne({username:username},callback)
+}
+function get_auth_userlist(callback){
+    userDB.collection('auth_users').find({}).toArray(callback);
+}
+function update_send_list(callback){
+    userDB.collection('auth_users').find({}).toArray(function(err,userlist){
+        userlist.forEach(function(item){
+            if(item && item.chat_id){
+                send_list.push(item.chat_id)
+            }
+        })
+    });
+}
+function send_new_post_info_to_send_list(post,callback){
+    if (send_list.length > 0){
+        send_list.forEach(function(chat_id){
+            try{
+                var fromId = chat_id;
+                var resp = JSON.stringify({url:'http://cdn.tiegushi.com/posts/'+json.postId,title:post.title,addonTitle:post.addontitle,
+                    ownerName:post.ownerName
+                });
+                bot.sendMessage(fromId, resp);
+            }catch(e){
+
+            }
+        })
+    }
+}
+
+bot.onText(/\/start/, function (msg, match) {
+    var fromId = msg.from.id;
+    var fromUsername = msg.from.username;
+    console.log(msg)
+    var usage = '*Command list:*\n' +
+        '*/auth [password]*  Add yourself to send list\n' +
+        '*/remove*                Remove yourself from send list\n' +
+        '*/all*                   Show all user in the send list\n';
+    bot.sendMessage(fromId, usage,{parse_mode:"Markdown"});
+});
+bot.onText(/\/all/, function (msg, match) {
+    var fromId = msg.from.id;
+    var fromUsername = msg.from.username;
+    check_if_legit_user(fromUsername,function(err,user){
+        if(!err && user && user.username===fromUsername){
+            get_auth_userlist(function(err,userlist){
+                console.log(userlist)
+                var resp = ''
+                var count = 0
+                userlist.forEach(function(item,index){
+                    if(item && item.username && item.chat_id){
+                        resp += item.username + '/' + item.chat_id + '  ';
+                        count ++;
+                    }
+                })
+                bot.sendMessage(fromId, resp + (count>1 ? 'are' : 'is') + ' in the send list', {parse_mode:"Markdown"});
+            })
+        } else {
+            bot.sendMessage(fromId, 'Please run /auth [password] first.',{parse_mode:"Markdown"});
+        }
+    });
+});
+bot.onText(/\/remove/, function (msg, match) {
+    var fromId = msg.from.id;
+    var fromUsername = msg.from.username;
+    check_if_legit_user(fromUsername,function(err,user){
+        if(!err && user && user.username===fromUsername){
+            remove_auth_user_info_to_db(fromUsername,fromId,function(){
+                update_send_list();
+            });
+            bot.sendMessage(fromId, "*You* ("+fromUsername+"/"+fromId+")  were successfully removed from send list.",{parse_mode:"Markdown"});
+        } else {
+            bot.sendMessage(fromId, 'Please run /auth [password] first.',{parse_mode:"Markdown"});
+        }
+    });
+});
+bot.onText(/\/auth (.+)/, function (msg, match) {
+    var fromId = msg.from.id;
+    var fromUsername = msg.from.username;
+    var passwd = match[1];
+
+    check_if_legit_user(fromUsername,function(err, user) {
+        console.log(user)
+        if (user){
+            if(user.chat_id === fromId){
+                bot.sendMessage(fromId, "*You* ("+fromUsername+"/"+fromId+") have already been in send list.",{parse_mode:"Markdown"});
+            } else {
+                bot.sendMessage(fromId, "You ("+fromUsername+"/"+fromId+") have already been in send list. Chat ID changed from "+
+                    user.chat_id +" to "+fromId,{parse_mode:"Markdown"});
+                update_auth_user_info_to_db(fromUsername,fromId,function(){
+                    update_send_list();
+                })
+            }
+        } else {
+            if(passwd && passwd === telegram_password){
+                bot.sendMessage(fromId, 'Your password is correct. Added to send list.',{parse_mode:"Markdown"});
+                add_auth_user_info_to_db(fromUsername,fromId,function(){
+                    update_send_list();
+                });
+            } else {
+                bot.sendMessage(fromId, 'Your password is mismatch with correct one, please check your input',{parse_mode:"Markdown"});
+            }
+        }
+    });
+});
+
 client.on('connect' ,function () {
     console.log('Connected to server')
     client.subscribe('postView',{qos:2});
@@ -63,67 +185,6 @@ client.on('connect' ,function () {
                 json.createdAt = new Date();
             }
             console.log('To save postview: '+JSON.stringify(json));
-            var fromId = 246713289;
-            db.collection('posts').findOne({_id:json.postId},{fields:{
-                browse:true,
-                title:true,
-                addontitle:true,
-                owner:true,
-                _id:true,
-                ownerName:true,
-                createdAt:true,
-                browse:true,
-                mainImage:true
-            }},function(err, post) {
-                if(!err && post){
-                    try{
-                        var fromId = 246713289;
-                        var resp = JSON.stringify({url:'http://cdn.tiegushi.com/posts/'+json.postId,title:post.title?post.title:'',addonTitle:post.addontitle,
-                            ownerName:post.ownerName,browse:post.browse
-                        });
-                        bot.sendMessage(fromId, resp);
-                        console.log(post)
-                    }catch(e){
-
-                    }
-                }
-            });
-            /*
-             db.collection('posts').findOne({_id:json.postId},{fields:{
-             browse:true,
-             title:true,
-             addontitle:true,
-             owner:true,
-             _id:true,
-             ownerName:true,
-             createdAt:true,
-             mainImage:true
-             }},function(err, post) {
-             //console.log(post);
-             db.collection('users').findOne({_id:json.userId},{fields:{
-             username: true,
-             createdAt:true,
-             'profile.fullname': true,
-             type: true,
-             'profile.sex':true,
-             'profile.lastLogonIP':true,
-             'profile.anonymous':true,
-             'profile.browser':true,
-             'profile.location':true
-             }},function(err, user) {
-             console.log(user.profile.fullname+' -[:VIEWER]-> '+post.title);
-             savePostUser.save_user_node(user,function(){
-             savePostUser.save_post_node(post,function(){
-             console.log('Info saved')
-             json.createdAt=new Date()
-             save_viewer_node(json,function(){
-
-             })
-             })
-             })
-             });
-
-             });*/
         } else if(topic === 'newUser'){
             db.collection('users').findOne({_id:json.userId},{fields:{
                 username: true,
@@ -150,20 +211,10 @@ client.on('connect' ,function () {
                 mainImage:true
             }},function(err, post) {
                 if(!err && post) {
-                    try{
-                        var fromId = 246713289;
-                        var resp = JSON.stringify({url:'http://cdn.tiegushi.com/posts/'+json.postId,title:post.title,addonTitle:post.addontitle,
-                            ownerName:post.ownerName
-                        });
-                        bot.sendMessage(fromId, resp);
-                        console.log(post)
-                    }catch(e){
-
-                    }
+                    send_new_post_info_to_send_list(post)
                 }
             });
         }
-        //client.end();
     });
 
     //client.publish('raid_cdn_add_torrent',JSON.stringify({token:"sXrfefPZ3CSHS2Mwu",url:"http://zifacdn.oss-cn-hangzhou.aliyuncs.com/yosemite-hd.mp4",videoLength:78396301,file_path:"/root/docker-share/sXrfefPZ3CSHS2Mwu/zifacdn.oss-cn-hangzhou.aliyuncs.com/yosemite-hd.mp4"} ));
