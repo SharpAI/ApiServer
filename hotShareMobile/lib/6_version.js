de23b25c4badb6e2452178923af825acbf68a6ae
sraita <sraita@139.com>
2016-09-18 18:06:54 +0800

    Bump Version to 1.3.25.

--- hotShareMobile/lib/6_version.js
+++ hotShareMobile/lib/6_version.js
@@ -1,1 +1,1 @@
-version_of_build = '1.3.24';
+version_of_build = '1.3.25';
--- hotShareWeb/mobile-config.js
+++ hotShareWeb/mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   id: 'org.hotshare.everywhere',
-  version: '1.3.24',
+  version: '1.3.25',
   name: 'hotShare-web',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
