ad88410cb8178562f9f629abe7518136c0275113
Simba Zhang <solderzzc@gmail.com>
2016-10-04 12:25:05 -0700

    Load 5 article of discover since the image will cost bandwidth.
    Change-Id: Idf57d8e6a72254501d02c93d39108c3cbba90032

--- hotShareWeb/public/statics/posts/showpost.js
+++ hotShareWeb/public/statics/posts/showpost.js
@@ -6,7 +6,7 @@
 
 
     var SUGGEST_POSTS_SKIP = 0;
-    var SUGGEST_POSTS_LIMIT = 10;
+    var SUGGEST_POSTS_LIMIT = 5;
     var SUGGEST_POSTS_LOADING = false;
     var predefineColors = ["#55303e", "#503f32", "#7e766c", "#291d13", "#d59a73", "#a87c5f", "#282632", "#ca9e92", "#a7a07d", "#846843", "#6ea89e", "#292523", "#637168", "#573e1b", "#925f3e", "#786b53", "#aaa489", "#a5926a", "#6a6b6d", "#978d69", "#a0a1a1", "#4b423c", "#5f4a36", "#b6a2a9", "#1c1c4e", "#e0d9dc", "#393838", "#c5bab3", "#a46d40", "#735853", "#3c3c39"];
 
