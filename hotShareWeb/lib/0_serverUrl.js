50a9bb83cf836bb6cd58517bc79dc4d25c8c8b0d
Simba Zhang <solderzzc@gmail.com>
2016-10-10 14:11:45 -0700

    Changed the quota location to reduce the misunderstooding.

--- hotShareWeb/lib/0_serverUrl.js
+++ hotShareWeb/lib/0_serverUrl.js
@@ -3,6 +3,6 @@ chat_server_url = 'chat.cdn.tiegushi.com';
 sign_server_url = 'http://sign.tiegushi.com:8080/sign/';
 import_server_url = 'http://urlanalyser.tiegushi.com:8080/import';
 import_cancel_url = 'http://urlanalyser.tiegushi.com:8080/import-cancel';
-ddp_alter_url = (typeof process !== "undefined" && process !== null ? ('"'+process.env.DDP_ALTER_URL+'"') : void 0) || "ws://localhost:5000/websocket";
+ddp_alter_url = (typeof process !== "undefined" && process !== null ? process.env.DDP_ALTER_URL : void 0) || 'ws://localhost:5000/websocket';
 // import_server_url = 'http://192.168.1.84:8080/import';
 // import_cancel_url = 'http://192.168.1.84:8080/import-cancel';
