630d24bf4364a5466b80e83918a2af6ef9d88dba
Gui Fubo <sraita@139.com>
2016-11-01 18:40:19 +0800

    Bump hotShare Version to 1.4.26 and bundle-post Version to 1.2.20 (#497)
    * Bump hotShare Version to 1.4.26 and bundle-post Version to 1.2.20

--- hotShareWeb/lib/6_version.js
+++ hotShareWeb/lib/6_version.js
@@ -1,1 +1,1 @@
-version_of_build = '1.4.24';
+version_of_build = '1.4.26';
--- hotShareWeb/mobile-config.js
+++ hotShareWeb/mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   id: 'org.hotshare.everywhere',
-  version: '1.4.24',
+  version: '1.4.26',
   name: 'hotShare-web',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
