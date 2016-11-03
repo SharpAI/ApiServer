be49fef79733ed20ae34edcb5ec4d95309ae7090
lambda-zhang <ggxxde@163.com>
2016-11-03 15:43:12 +0800

    syncService使用oplog同步数据，不使用mqtt (#512)

--- SocialGraphic/opSync/main.js
+++ SocialGraphic/opSync/main.js
@@ -1,19 +1,19 @@
-/**
- * Created by simba on 5/13/16.
- */
-
-var mqtt    = require('mqtt');
+var async = require('async');
+var assert = require('assert');
 var savePostUser = require('./import-user-post-info');
 var save_viewer_node = require('./import-viewer-info').save_viewer_node;
-//var client  = mqtt.connect('mqtt://broker.mqttdashboard.com');
-//var client  = mqtt.connect('mqtt://broker.raidcdn.org');
-var client  = mqtt.connect('ws://rpcserver.raidcdn.com:80');
-//var client  = mqtt.connect('mqtt://rpcserver.raidcdn.com');
-var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
-var url = 'mongodb://hotShareAdmin:aei_19056@host1.tiegushi.com:27017/hotShare';
-//var url = 'mongodb://localhost:27017/localdb';
+var MongoOplog = require('mongo-oplog');
+var conn = {
+  mongo: 'mongodb://hotShareAdmin:aei_19056@host1.tiegushi.com:27017/hotShare',
+  oplog: 'mongodb://oplogger:PasswordForOplogger@host1.tiegushi.com:27017/local?authSource=admin',
+  //FIXME: why not filter?  to much network traffic
+  oplog_opts_v: { ns: 'hotShare.viewers' , server : { reconnectTries : 3000, reconnectInterval: 2000, autoReconnect : true }},
+  oplog_opts_p: { ns: 'hotShare.posts'   , server : { reconnectTries : 3000, reconnectInterval: 2000, autoReconnect : true }},
+  oplog_opts_u: { ns: 'hotShare.users'   , server : { reconnectTries : 3000, reconnectInterval: 2000, autoReconnect : true }},
+};
 var db = null
+var MongoClient = require('mongodb').MongoClient;
+
 process.addListener('uncaughtException', function (err) {
     var msg = err.message;
     if (err.stack) {
@@ -25,104 +25,234 @@ process.addListener('uncaughtException', function (err) {
     console.log(msg);
     console.trace();
 });
-MongoClient.connect(url, function(err, tdb) {
+MongoClient.connect(conn.mongo, function(err, tdb) {
     assert.equal(null, err);
     db=tdb
 });
-client.on('connect' ,function () {
-    console.log('Connected to server')
-    client.subscribe('postView',{qos:2});
-    client.subscribe('publishPost',{qos:2});
-    client.subscribe('newUser',{qos:2});
+mongo_connect();
 
-    client.on('message', function (topic, message) {
-        // message is Buffer
-        console.log(topic+': '+message.toString());
-        var json = JSON.parse(message)
-        if(topic === 'postView'){
-            if (!json.userId || !json.postId){
-                return
-            }
-            if (json.postId.indexOf('?')>0){
-                json.postId = json.postId.split('?')[0];
-            }
-            if (!json.createdAt){
-                json.createdAt = new Date();
-            }
-            console.log('To save postview: '+JSON.stringify(json));
-            save_viewer_node(json,function(){
-            })
-            /*
-            db.collection('posts').findOne({_id:json.postId},{fields:{
-                browse:true,
-                title:true,
-                addontitle:true,
-                owner:true,
-                _id:true,
-                ownerName:true,
-                createdAt:true,
-                mainImage:true
-            }},function(err, post) {
-                //console.log(post);
-                db.collection('users').findOne({_id:json.userId},{fields:{
-                    username: true,
-                    createdAt:true,
-                    'profile.fullname': true,
-                    type: true,
-                    'profile.sex':true,
-                    'profile.lastLogonIP':true,
-                    'profile.anonymous':true,
-                    'profile.browser':true,
-                    'profile.location':true
-                }},function(err, user) {
-                    console.log(user.profile.fullname+' -[:VIEWER]-> '+post.title);
-                    savePostUser.save_user_node(user,function(){
-                        savePostUser.save_post_node(post,function(){
-                            console.log('Info saved')
-                            json.createdAt=new Date()
-                            save_viewer_node(json,function(){
+function mongo_connect() {
+  var oplog_v = MongoOplog(conn.oplog, conn.oplog_opts_v).tail();
+  oplog_v.on('op', function (data) {
+    get_id(data);
+  });
+  oplog_v.on('error', function (error) {
+    console.log('>>> error: ' + error);
+  });
+  oplog_v.on('end', function () {
+    console.log('>>> end: Stream ended');
+  });
+  oplog_v.stop(function () {
+    console.log('>>> stop: server stopped');
+  });
 
-                            })
-                        })
-                    })
-                });
+  var oplog_p = MongoOplog(conn.oplog, conn.oplog_opts_p).tail();
+  oplog_p.on('op', function (data) {
+    get_id(data);
+  });
+  oplog_p.on('error', function (error) {
+    console.log('>>> error: ' + error);
+  });
+  oplog_p.on('end', function () {
+    console.log('>>> end: Stream ended');
+  });
+  oplog_p.stop(function () {
+    console.log('>>> stop: server stopped');
+  });
 
-            });*/
-        } else if(topic === 'newUser'){
-            db.collection('users').findOne({_id:json.userId},{fields:{
-                username: true,
-                createdAt:true,
-                'profile.fullname': true,
-                type: true,
-                'profile.sex':true,
-                'profile.lastLogonIP':true,
-                'profile.anonymous':true,
-                'profile.browser':true,
-                'profile.location':true
-            }},function(err, user) {
-                savePostUser.save_user_node(user,function(){
-                    console.log('User Info saved')
-                })
-            });
-        } else if(topic === 'publishPost'){
-            db.collection('posts').findOne({_id:json.postId},{fields:{
-                browse:true,
-                title:true,
-                addontitle:true,
-                owner:true,
-                _id:true,
-                ownerName:true,
-                createdAt:true,
-                mainImage:true
-            }},function(err, post) {
-                savePostUser.save_post_node(post,function(){
-                    console.log('Post Info saved')
-                })
-            });
+  var oplog_u = MongoOplog(conn.oplog, conn.oplog_opts_u).tail();
+  oplog_u.on('op', function (data) {
+    get_id(data);
+  });
+  oplog_u.on('error', function (error) {
+    console.log('>>> error: ' + error);
+  });
+  oplog_u.on('end', function () {
+    console.log('>>> end: Stream ended');
+  });
+  oplog_u.stop(function () {
+    console.log('>>> stop: server stopped');
+  });
+}
+
+function get_id(doc) {
+  var postId = null;
+  var userId = null;
+  var viewerId = null;
+  var postDoc = null;
+  var userDoc = null;
+  var viewerDoc = null;
+
+  //console.log('>>> ' + JSON.stringify(doc))
+  async.series({
+    getid: function(callback){
+      if(doc.op === 'i') {
+        if(doc.ns === conn.oplog_opts_v.ns && (!!doc.o) && doc.o.postId && doc.o.userId) {
+          postId = doc.o.postId
+          userId = doc.o.userId
+          viewerDoc = doc.o
+          viewerId = doc.o._id
         }
-        //client.end();
-    });
+        else if(doc.ns === conn.oplog_opts_u.ns && (!!doc.o) && doc.o._id)
+          userId = doc.o._id
+        else if(doc.ns === conn.oplog_opts_p.ns && (!!doc.o) && doc.o._id)
+          postId = doc.o._id
+        else
+          console.log('!!! unknow insert op: ' + JSON.stringify(doc))
 
-    //client.publish('raid_cdn_add_torrent',JSON.stringify({token:"sXrfefPZ3CSHS2Mwu",url:"http://zifacdn.oss-cn-hangzhou.aliyuncs.com/yosemite-hd.mp4",videoLength:78396301,file_path:"/root/docker-share/sXrfefPZ3CSHS2Mwu/zifacdn.oss-cn-hangzhou.aliyuncs.com/yosemite-hd.mp4"} ));
-    // client.publish('raid_cdn_add_torrent',JSON.stringify({token:"token",url:"http://zifacdn.oss-cn-hangzhou.aliyuncs.com/yosemite-hd.mp4",videoLength:78396301}));
-});
+        callback(null, 'insert ' + doc.ns + ' pid=' + postId + ' uid=' + userId);
+      }
+      else if(doc.op === 'u') {
+        if(doc.ns === conn.oplog_opts_v.ns && (!!doc.o2) && doc.o2._id) {
+          get_doc(doc.ns, doc.o2._id, function(ns, result) {
+            if(result) {
+              postId = result.postId
+              userId = result.userId
+              viewerDoc = result
+              viewerId = result._id
+            }
+            callback(null, 'update ' + doc.ns + ' pid=' + postId + ' uid=' + userId);
+          })
+        }
+        else if(doc.ns === conn.oplog_opts_u.ns && (!!doc.o2) && doc.o2._id) {
+          userId = doc.o2._id
+          callback(null, 'update ' + doc.ns + ' pid=' + postId + ' uid=' + userId);
+        }
+        else if(doc.ns === conn.oplog_opts_p.ns && (!!doc.o2) && doc.o2._id) {
+          postId = doc.o2._id
+          callback(null, 'update ' + doc.ns + ' pid=' + postId + ' uid=' + userId);
+        }
+        else {
+          console.log('!!! unknow update op: ' + JSON.stringify(doc))
+          callback(null, 'update ' + doc.ns + ' pid=' + postId + ' uid=' + userId);
+        }
+      }
+      else {
+        callback(null, doc.ns + 'other op: ' + JSON.stringify(doc));
+      }
+    },
+    getdoc: function(callback){
+      if(doc.ns === conn.oplog_opts_v.ns) {
+        get_doc('hotShare.users', userId, function(ns1, result1) {
+          if(result1)
+            userDoc = result1;
+          get_doc('hotShare.posts', postId, function(ns2, result2) {
+            if(result2)
+              postDoc = result2;
+
+            callback(null, doc.ns + ' get doc');
+          })
+        })
+      }
+      else if(doc.ns === conn.oplog_opts_u.ns) {
+        get_doc('hotShare.users', userId, function(ns1, result1) {
+          if(result1)
+            userDoc = result1;
+
+          callback(null, doc.ns + ' get doc');
+        })
+      }
+      else if(doc.ns === conn.oplog_opts_p.ns) {
+        get_doc('hotShare.posts', postId, function(ns2, result2) {
+          if(result2)
+            postDoc = result2;
+
+          callback(null, doc.ns + ' get doc');
+        })
+      }
+      else {
+        callback(null, doc.ns + ' not get doc');
+      }
+    }
+  },function(err, results) {
+    console.log(results);
+    //console.log('>>> ' + doc.ns + '   \t' + JSON.stringify(postDoc) + ' \t' + JSON.stringify(userDoc) + '\n\n')
+    sync_to_neo4j(doc.ns, postDoc, userDoc, viewerDoc);
+  });
+}
+
+function get_doc(ns, id, cb) {
+  if(!(!!ns && !!id)) {
+    return cb(null, null);
+  }
+
+  if(ns === conn.oplog_opts_v.ns) {
+    db.collection('viewers').findOne({_id:id},{fields:{
+      postId: true,
+      userId: true
+    }},function(err, viewer) {
+        if(err || (!viewer))
+          return cb(null, null)
+        else
+          return cb(ns, viewer)
+    });
+  }
+  else if(ns === conn.oplog_opts_u.ns) {
+    db.collection('users').findOne({_id:id},{fields:{
+      username: true,
+      createdAt:true,
+      'profile.fullname': true,
+      type: true,
+      'profile.sex':true,
+      'profile.lastLogonIP':true,
+      'profile.anonymous':true,
+      'profile.browser':true,
+      'profile.location':true
+    }},function(err, user) {
+        if(err || (!user))
+          return cb(null, null)
+        else
+          return cb(ns, user)
+    });
+  }
+  else if(ns === conn.oplog_opts_p.ns) {
+    db.collection('posts').findOne({_id:id},{fields:{
+      browse:true,
+      title:true,
+      addontitle:true,
+      owner:true,
+      _id:true,
+      ownerName:true,
+      createdAt:true,
+      mainImage:true
+    }},function(err, post) {
+        if(err || (!post))
+          return cb(null, null)
+        else
+          return cb(ns, post)
+    });
+  }
+  else
+    return cb(null, null);
+}
+
+function sync_to_neo4j(ns, postDoc, userDoc, viewerDoc) {
+  if(ns === conn.oplog_opts_v.ns) {
+    if(postDoc && postDoc._id && userDoc && userDoc._id)
+      savePostUser.save_user_node(userDoc,function(){
+        savePostUser.save_post_node(postDoc,function(){
+          save_viewer_node(viewerDoc, function(){
+            console.log('postview saved: pid=' + postDoc._id + ' uid=' + userDoc._id)
+          })
+        })
+      })
+    else
+      console.log(ns + ', failed with: id=' + postDoc._id + ' uid=' + userDoc._id)
+  }
+  else if(ns === conn.oplog_opts_u.ns) {
+    if(userDoc && userDoc._id)
+      savePostUser.save_user_node(userDoc,function(){
+        console.log('User Info saved: uid=' + userDoc._id)
+      })
+    else
+      console.log(ns + ', failed with: id=' + postDoc._id + ' uid=' + userDoc._id)
+  }
+  else if(ns === conn.oplog_opts_p.ns) {
+    if(postDoc && postDoc._id)
+      savePostUser.save_post_node(postDoc,function(){
+        console.log('Post Info saved: pid=' + postDoc._id)
+      })
+    else
+      console.log(ns + ', failed with: id=' + postDoc._id + ' uid=' + userDoc._id)
+  }
+}
