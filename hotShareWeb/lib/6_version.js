57d83ba855d02eed82c6686611e02e63e2f5e91d
Gui Fubo <sraita@139.com>
2016-11-04 18:20:45 +0800

    Bump hotShare Version to 1.4.29 and bundle Version to 1.2.22 (#520)

--- hotShareWeb/lib/6_version.js
+++ hotShareWeb/lib/6_version.js
@@ -1,1 +1,1 @@
-version_of_build = '1.4.27';
+version_of_build = '1.4.29';
--- hotShareWeb/mobile-config.js
+++ hotShareWeb/mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   id: 'org.hotshare.everywhere',
-  version: '1.4.27',
+  version: '1.4.29',
   name: 'hotShare-web',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
