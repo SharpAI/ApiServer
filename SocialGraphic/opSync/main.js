4da85380d0458d5d3c193c9b85ad335b3aa78616
lambda-zhang <ggxxde@163.com>
2016-11-16 15:22:48 +0800

    opSync: LIVING_PORT 8083 (#645)
    * reconnect after connection stoped
    
    * opSync: LIVING_PORT 8083

--- SocialGraphic/opSync/main.js
+++ SocialGraphic/opSync/main.js
@@ -273,6 +273,6 @@ server.get('/', function (req, res, next) {
   return next();
 });
 
-server.listen(8080, function () {
+server.listen(process.env.LIVING_PORT, function () {
   console.log('%s listening at %s', server.name, server.url);
 });
