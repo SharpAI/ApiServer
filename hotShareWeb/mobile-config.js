ceb494e4f5f73c51f6242ef44f66c636ce7c746c
Gui Fubo <sraita@139.com>
2016-11-16 18:05:14 +0800

    Bump hotShare Version to 1.4.40 . (#649)

--- hotShareWeb/mobile-config.js
+++ hotShareWeb/mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   id: 'org.hotshare.everywhere',
-  version: '1.4.39',
+  version: '1.4.40',
   name: 'hotShare-web',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
