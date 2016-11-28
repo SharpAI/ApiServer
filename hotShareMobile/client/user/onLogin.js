784fdb03e27dca0b561123721093ce32cd21eb0d
Doris1994 <dsun@actiontec.com>
2016-11-28 15:34:59 +0800

    Fixed TypeError- undefined is not an object (evaluating 'window.plugins.pushNotification.register') (#762)

--- hotShareMobile/client/user/onLogin.js
+++ hotShareMobile/client/user/onLogin.js
@@ -14,7 +14,7 @@ if (Meteor.isClient) {
         }
         console.log("Accounts.onLogin");
         Meteor.subscribe("pcomments");
-	    checkShareUrl();
+	      checkShareUrl();
         if(device.platform === 'Android'){
           window.plugins.shareExtension.getShareData(function(data) {
             console.log("##RDBG getShareData: " + JSON.stringify(data));
@@ -28,20 +28,44 @@ if (Meteor.isClient) {
         if (device.platform === 'iOS' && localStorage.getItem('registrationID') == null ) {
           var registerInterval1 = window.setInterval( function(){
             console.log('on push notification init');
-            window.plugins.pushNotification.register(function(result) {
+            var push = PushNotification.init({
+              ios: {
+                alert: "true",
+                badge: "true",
+                sound: "true",
+                clearBadge: "true"
+              }
+            });
+
+            push.on('registration', function (data) {
+              // data.registrationId
+              result = data.registrationId;
               console.log('Got registrationID ' + result);
               Session.set('registrationID', result);
               Session.set('registrationType', 'iOS');
               localStorage.setItem('registrationID', result);
               window.clearInterval(registerInterval1);
               return window.updatePushNotificationToken('iOS', result);
-            }, function(error) {
-              return console.log('No Push Notification support in this build error = ' + error);
-            }, {
-              "badge": "true",
-              "sound": "true",
-              "alert": "true",
-              "ecb": "window.onNotificationAPN"
+            });
+
+            push.on('notification', function (data) {
+              console.log('Got message');
+              if (data.count) {
+                Session.set('waitReadCount', data.count);
+              }
+              if (data.additionalData.foreground === false) {
+                console.log('Push notification when background');
+                window.refreshMainDataSource();
+                return;
+              }
+              if (data.message) {
+                PUB.toast(data.message);
+                return window.refreshMainDataSource();
+              }
+            });
+
+            push.on('error', function (e) {
+              return console.log('No Push Notification support in this build error = ' + e.message);
             });
            },20000 );
         }
