831683cf7a4890e29e258af110e780e498f1431a
Zhiyue <zhiyuelive@outlook.com>
2016-12-27 10:16:12 +0800

    Bump version to 1.4.70. (#987)

--- hotShareWeb/mobile-config.js
+++ hotShareWeb/mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   id: 'org.hotshare.everywhere',
-  version: '1.4.64',
+  version: '1.4.70',
   name: 'hotShare-web',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
