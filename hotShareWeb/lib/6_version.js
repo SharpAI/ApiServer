eb752235cce2a5495166a93e22f7c86289eec0c7
Gui Fubo <sraita@139.com>
2016-12-28 15:35:58 +0800

    Bump Version to 1.4.72 (#996)

--- hotShareWeb/lib/6_version.js
+++ hotShareWeb/lib/6_version.js
@@ -1,1 +1,1 @@
-version_of_build = '1.4.71';
+version_of_build = '1.4.72';
--- hotShareWeb/mobile-config.js
+++ hotShareWeb/mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   id: 'org.hotshare.everywhere',
-  version: '1.4.71',
+  version: '1.4.72',
   name: 'hotShare-web',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
