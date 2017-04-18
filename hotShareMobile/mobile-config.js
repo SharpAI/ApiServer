95fb352ac88ce14069e32001eeae27ef505c8b80
Simba Zhang <solderzzc@gmail.com>
2017-04-18 11:08:47 -0700

    Bump version to 0.9.1

--- hotShareMobile/mobile-config.js
+++ hotShareMobile/mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   id: 'org.workai.everywhere',
-  version: '0.1.13',
+  version: '0.9.1',
   name: 'WorkAI',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
