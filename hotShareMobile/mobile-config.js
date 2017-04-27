45029de2e7760d0817e06e95ca899c680308f690
ningning-718 <ningning-718@users.noreply.github.com>
2017-04-27 17:49:19 +0800

    bump to version 1.0.7 (#1605)
    Change-Id: If47b3f1cc580272d59cca26be8942de99d17d9f9

--- hotShareMobile/mobile-config.js
+++ hotShareMobile/mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   id: 'org.workai.everywhere',
-  version: '1.0.6',
+  version: '1.0.7',
   name: 'WorkAI',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
--- hotShareWeb/mobile-config.js
+++ hotShareWeb/mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   id: 'org.workai.everywhere',
-  version: '1.0.6',
+  version: '1.0.7',
   name: 'WorkAI-web',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
