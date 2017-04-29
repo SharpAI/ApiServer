48ae164a2bfc16a2b517dde92632e9301087eb9c
Simba Zhang <solderzzc@gmail.com>
2017-04-28 18:25:18 -0700

    To sync sendMsgToChatRoom(the behavior simulator to mobile project). Added script to copy the app icon resource into build dir.

--- hotShareMobile/build.sh
+++ hotShareMobile/build.sh
@@ -5,4 +5,5 @@ rm -rf .meteor/local/build
 rm -rf .meteor/local/bundler-cache
 rm -rf .meteor/local/plugin-cache
 meteor build ~/build-workai --server=http://workaihost.tiegushi.com
+cp -rf ../WorkAI_Assets/* ~/build-workai/ios/project/WorkAI/Images.xcassets/
 # meteor build ~/build-hot-share --server=http://storeboard.tiegushi.com
