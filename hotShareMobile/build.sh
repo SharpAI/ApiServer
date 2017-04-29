46f75bd61af841135acc06b1301b6e78b0ff4fbe
Simba Zhang <solderzzc@gmail.com>
2017-04-28 19:07:11 -0700

    update build.sh to delete project/WorkAI/Images.xcassets/ before copy.

--- hotShareMobile/build.sh
+++ hotShareMobile/build.sh
@@ -5,5 +5,6 @@ rm -rf .meteor/local/build
 rm -rf .meteor/local/bundler-cache
 rm -rf .meteor/local/plugin-cache
 meteor build ~/build-workai --server=http://workaihost.tiegushi.com
+rm -rf ~/build-workai/ios/project/WorkAI/Images.xcassets/*
 cp -rf ../WorkAI_Assets/* ~/build-workai/ios/project/WorkAI/Images.xcassets/
 # meteor build ~/build-hot-share --server=http://storeboard.tiegushi.com
