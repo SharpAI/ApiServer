c38be0d3c5da2bac64cfdc494e0825b992f53947
Doris1994 <dsun@actiontec.com>
2017-05-04 18:05:25 +0800

    Bump version to 1.7.1 (#1640)

--- hotShareMobile/mobile-config.js
+++ hotShareMobile/mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   id: 'org.workai.everywhere',
-  version: '1.7.0',
+  version: '1.7.1',
   name: 'WorkAI',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
