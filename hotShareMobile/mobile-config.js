1d6f1017aac2c9cedcadf457ba5c4e0a25421e7e
lambda-zhang <ggxxde@163.com>
2017-05-08 21:23:47 +0800

    Bump version to 1.7.2 (#1667)

--- hotShareMobile/mobile-config.js
+++ hotShareMobile/mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   id: 'org.workai.everywhere',
-  version: '1.7.1',
+  version: '1.7.2',
   name: 'WorkAI',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
