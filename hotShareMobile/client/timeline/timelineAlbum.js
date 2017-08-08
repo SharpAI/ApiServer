3dc2b0724a732ccf7d326a795bcceff4c4e1a713
sraita <sraita@139.com>
2017-08-08 11:11:08 +0800

    remove test data

--- hotShareMobile/client/timeline/timelineAlbum.js
+++ hotShareMobile/client/timeline/timelineAlbum.js
@@ -20,35 +20,10 @@ Template.timelineAlbum.helpers({
     var uuid = Router.current().params._uuid;
     var msgs = [];
     var tempDate = null;
-    var list = [{
-      "images":[{
-        "url":"http://workaiossqn.tiegushi.com/03c6ed68-7b53-11e7-a2fd-7427eaa513de"
-      }],
-      "create_time":"2017-08-07T09:30:11.362Z"
-    },{
-      "images":[{
-        "url":"http://workaiossqn.tiegushi.com/03c6ed68-7b53-11e7-a2fd-7427eaa513de"
-      },{
-        "url":"http://workaiossqn.tiegushi.com/03c6ed68-7b53-11e7-a2fd-7427eaa513de"
-      },{
-        "url":"http://workaiossqn.tiegushi.com/03c6ed68-7b53-11e7-a2fd-7427eaa513de"
-      },{
-        "url":"http://workaiossqn.tiegushi.com/03c6ed68-7b53-11e7-a2fd-7427eaa513de"
-      },{
-        "url":"http://workaiossqn.tiegushi.com/03c6ed68-7b53-11e7-a2fd-7427eaa513de"
-      }],
-      "create_time":"2017-08-07T09:32:11.362Z"
-    },{
-      "images":[{
-        "url":"http://workaiossqn.tiegushi.com/03c6ed68-7b53-11e7-a2fd-7427eaa513de"
-      }],
-      "create_time":"2017-08-07T09:32:40.362Z"
-    }]
     if(SimpleChat.Messages.find({people_uuid:uuid,type:'text', images: {$exists: true}},{sort:{create_time:1}}).count() ==0){
        SimpleChat.withMessageHisEnable && SimpleChat.loadMoreMesage({people_uuid:uuid,type:'text', images: {$exists: true}}, {limit: 10, sort: {create_time: -1}}, 10);
     }
     SimpleChat.Messages.find({people_uuid:uuid,type:'text', images: {$exists: true}},{sort:{create_time:-1}}).forEach(function(item){
-    // list.forEach(function(item){
       var date = new Date(item.create_time);
       var time = date.shortTime();
       var obj = {
