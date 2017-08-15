190de395be8ef20174590aa3187a78241e3697a1
Doris1994 <dsun@actiontec.com>
2017-08-15 15:14:25 +0800

    fix check_in_out person name; (#2475)

--- hotShareWeb/packages/simple-chat/server/method.js
+++ hotShareWeb/packages/simple-chat/server/method.js
@@ -459,13 +459,16 @@ Meteor.methods({
       console.log('person info:'+person.name);
     }
     else{
-      var relation = WorkAIUserRelations.findOne({'app_user_id':user._id,'group_id':person_info.group_id});
-      var person_name = '';
-      if (relation && relation.person_name) {
-        person_name = relation.person_name;
-      }
-      else{
-        return {result:'error',reason:'请选择一张有名字的照片或前往聊天室进行标记~'};
+      var person_name = person_info.name;
+      if (!person_name) {
+        var relation = WorkAIUserRelations.findOne({'app_user_id':user._id,'group_id':person_info.group_id});
+        
+        if (relation && relation.person_name) {
+          person_name = relation.person_name;
+        }
+        else{
+          return {result:'error',reason:'请选择一张有名字的照片或前往聊天室进行标记~'};
+        }
       }
       person = PERSON.setName(person_info.group_id, person_info.uuid, data.face_id, person_info.img_url, person_name);
     }
