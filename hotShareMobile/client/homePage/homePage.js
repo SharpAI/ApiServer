c761027d775e508f3e42b5c1b6d1d8af0da8389d
sraita <sraita@139.com>
2017-08-21 16:23:23 +0800

    代签无头像（client）

--- hotShareMobile/client/homePage/homePage.js
+++ hotShareMobile/client/homePage/homePage.js
@@ -175,6 +175,7 @@ modifyStatusFun = function(group_id,in_out,taId){
     if(deviceCount === 1){
       var device = Devices.findOne({groupId: group_id,in_out:in_out},{sort:{createAt:-1}})
       if(taId){
+        Session.set('wantModify',true);
         return PUB.page('/timelineAlbum/'+device.uuid+'?taId='+taId);
       }
       return PUB.page('/timelineAlbum/'+device.uuid);
@@ -196,6 +197,7 @@ Template.homePage.events({
     var taId = Session.get('modifyMyStatus_ta_id');
     var pageUrl = '/timelineAlbum/'+e.currentTarget.id;
     if(taId){
+      Session.set('wantModify',true);
       pageUrl = '/timelineAlbum/'+e.currentTarget.id+'?taId='+taId;
     }
     setTimeout(function(){
