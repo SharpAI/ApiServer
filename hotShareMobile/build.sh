2b2be9cd768899ea833bda2c834dd84be7c737e9
ningning-718 <ningning-718@users.noreply.github.com>
2017-08-28 12:28:27 +0800

    set default env as test, just as what is in 1_serverUrl.js (#2583)
    Change-Id: I85ade2a47303ab29bebddb6e4f03386d11ed6c09

--- hotShareMobile/build.sh
+++ hotShareMobile/build.sh
@@ -4,9 +4,9 @@ rm -rf .meteor/local/cordova-build
 rm -rf .meteor/local/build
 rm -rf .meteor/local/bundler-cache
 rm -rf .meteor/local/plugin-cache
-meteor build ~/build-workai --server=http://workaihost.tiegushi.com
+#meteor build ~/build-workai --server=http://workaihost.tiegushi.com
 #测试版
-#meteor build ~/build-workai --server=http://testworkai.tiegushi.com
+meteor build ~/build-workai --server=http://testworkai.tiegushi.com
 rm -rf ~/build-workai/ios/project/WorkAI/Images.xcassets/*
 cp -rf ../WorkAI_Assets/* ~/build-workai/ios/project/WorkAI/Images.xcassets/
 
