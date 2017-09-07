d354bb1028cd032191b158347cd4cf26ea0c1323
Doris1994 <dsun@actiontec.com>
2017-09-07 17:50:28 +0800

    Bump version to 2.8.4 (#2653)

--- hotShareMobile/lib/6_version.js
+++ hotShareMobile/lib/6_version.js
@@ -1,1 +1,1 @@
-version_of_build = '2.8.3';
+version_of_build = '2.8.4';
--- hotShareMobile/mobile-config.js
+++ hotShareMobile/mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   id: 'org.workai.everywhere',
-  version: '2.8.3',
+  version: '2.8.4',
   name: 'WorkAI',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
