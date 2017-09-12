52f3c2357101ff26ca5ce3766a59458802d35f41
sraita <sraita@139.com>
2017-09-12 11:54:29 +0800

    离开公司的pop up提醒：1. 如果time相同， 点击过后， 不再弹出第二次； 2. 10 分钟内，只提醒用户一次（点击过后）

--- hotShareMobile/lib/user-checkout.js
+++ hotShareMobile/lib/user-checkout.js
@@ -95,14 +95,31 @@ if (Meteor.isServer){
     }
   });
 } else if (Meteor.isClient){
-  var showConfirm = function(time){
+  var showConfirm = function(time,tryAgain){
+    var latest_confirm = localStorage.getItem('latest_user_checkout_confirm') || 0;
+    var latest_checkout_time = localStorage.getItem('latest_user_checkout_log_time');
+    var now = Date.now();
+    
+    // 如果time相同， 点击过后， 不再弹出第二次
+    if(!tryAgain && latest_checkout_time === time){
+      return;
+    }
+    // 10 分钟内,提示信息不重复弹出
+    if (!tryAgain && (now - latest_confirm) < (10*60*1000)){
+      return;
+    }
+
     Template._user_checkout_confirm.open('系统于 '+time.toLocaleString()+' 检测到你离开了公司，请确认是否已经下班了?', function(result){
+
+      localStorage.setItem('latest_user_checkout_confirm',Date.now());
+      localStorage.setItem('latest_user_checkout_log_time', time);
+      
       if (result){
         Meteor.call('upUCS', function(err1, res1){
           console.log('您确定已经下班了吗？', (!err1 || !res1) ? 'succ' : 'error');
           if (err1 || !res1){
             PUB.alert('操作失败，请重试~', function(){
-              showConfirm(time);
+              showConfirm(time, true);
             });
           }
         });
@@ -120,7 +137,7 @@ if (Meteor.isServer){
 
       if (Meteor.userId() && UserCheckoutEndLog.find({userId: Meteor.userId()}).count() > 0){
         var checkout_log = UserCheckoutEndLog.findOne({userId: Meteor.userId()});
-        var now = checkout_log && checkout_log.params && checkout_log.params.msg_data && checkout_log.params.msg_data.create_time ? checkout_log.params.msg_data.create_time : new Date();
+        var now = checkout_log && checkout_log.params && checkout_log.params.msg_data && checkout_log.params.msg_data.create_time ? checkout_log.params.msg_data.create_time : new Date(); 
         showConfirm(now);
       }
     });
