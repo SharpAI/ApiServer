fb62f22dd9f1d72182c15373702e655ce9f04150
sraita <sraita@139.com>
2017-10-12 10:17:17 +0800

    fix error on getOfficesOnlineInfo

--- hotShareWeb/server/veworld_server.js
+++ hotShareWeb/server/veworld_server.js
@@ -5,12 +5,14 @@ if(Meteor.isServer){
         var lists = [];
         ids.forEach(function(item){
           var group = SimpleChat.Groups.findOne({_id: item});
-          var count = WorkStatus.find({date:date,group_id: item,status: 'in'}).count();
-          lists.push({
-            _id: group._id,
-            name: group.name,
-            value: count
-          });
+          if(group){
+            var count = WorkStatus.find({date:date,group_id: item,status: 'in'}).count();
+            lists.push({
+              _id: group._id,
+              name: group.name,
+              value: count
+            });
+          }
         });
         return lists;
       }
