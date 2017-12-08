aba2bc870c04a126e9e7ecdcb16fe10f2dfa79c4
sraita <sraita@139.com>
2017-12-08 14:43:58 +0800

    Bump Version to 3.1.9.

--- hotShareWeb/lib/6_version.js
+++ hotShareWeb/lib/6_version.js
@@ -1,1 +1,1 @@
-version_of_build = '3.1.8';
+version_of_build = '3.1.9';
--- hotShareWeb/mobile-config.js
+++ hotShareWeb/mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   id: 'org.workai.everywhere',
-  version: '3.1.8',
+  version: '3.1.9',
   name: 'WorkAI-web',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
