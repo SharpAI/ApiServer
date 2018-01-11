5331c1d0501cff82087e682cd28d4493333cc702
sraita <sraita@139.com>
2018-01-11 14:51:41 +0800

    fix:device dashboard image and time  can not auto update

--- hotShareMobile/client/deviceDashboard/deviceDashboard.js
+++ hotShareMobile/client/deviceDashboard/deviceDashboard.js
@@ -1,5 +1,4 @@
-var theCurrentDay = new ReactiveVar(null);
-var theDisplayDay = new ReactiveVar(null);
+var date = new ReactiveVar(null);
 var today         = new ReactiveVar(null);
 
 var group        = new ReactiveVar({});
@@ -8,19 +7,18 @@ var isOut        = new ReactiveVar(true);
 
 Template.deviceDashboard.onRendered(function () {
   var now = new Date();
-  var displayDate = new Date(now.getFullYear(), now.getMonth(), now.getDate()).getTime();
-  var date = Date.UTC(now.getFullYear(),now.getMonth(), now.getDate() , 
+  var _today = new Date(now.getFullYear(), now.getMonth(), now.getDate()).getTime();
+  var _date = Date.UTC(now.getFullYear(),now.getMonth(), now.getDate() , 
       0, 0, 0, 0);
 
   var group_id = Router.current().params.group_id;
 
-  theCurrentDay.set(date); //UTC日期
-  theDisplayDay.set(displayDate); //当前显示的日期
-  today.set(displayDate);
+  date.set(_date); //UTC日期
+  today.set(_today);
 
-  Meteor.subscribe('WorkStatusByGroup',date, group_id,{
+  Meteor.subscribe('WorkStatusByGroup',date.get(), group_id,{
      onReady:function(){
-      var _lists = WorkStatus.find({group_id: group_id, date: date}).fetch();
+      var _lists = WorkStatus.find({group_id: group_id, date: date.get()}).fetch();
       console.log(_lists);
       lists.set(_lists);
     }
@@ -30,7 +28,17 @@ Template.deviceDashboard.onRendered(function () {
 
 Template.deviceDashboard.helpers({
   lists: function(){
-    return lists.get();
+    var now = new Date();
+    var _today = new Date(now.getFullYear(), now.getMonth(), now.getDate()).getTime();
+    var _date = Date.UTC(now.getFullYear(),now.getMonth(), now.getDate() , 
+        0, 0, 0, 0);
+
+    var group_id = Router.current().params.group_id;
+
+    date.set(_date); //UTC日期
+    today.set(_today);
+
+    return WorkStatus.find({group_id: group_id, date: date.get()}).fetch();
   },
   groupName: function(){
     return Session.get('deviceDashboardTitle');
