5b5aa187c8d40bad9aa50233913a51a81bf53dc6
sraita <guifubo@msn.cn>
2018-03-20 11:52:34 +0800

    modify the client app build bash

--- hotShareMobile/build.sh
+++ hotShareMobile/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-rm -rf ~/build-workai
+rm -rf ~/build-sharpai
 rm -rf .meteor/local/cordova-build
 rm -rf .meteor/local/build
 rm -rf .meteor/local/bundler-cache
 rm -rf .meteor/local/plugin-cache
-meteor build ~/build-workai --server=http://workaihost.tiegushi.com
+meteor build ~/build-sharpai --server=http://workaihost.tiegushi.com
 #测试版
 #meteor build ~/build-workai --server=http://testworkai.tiegushi.com
-rm -rf ~/build-workai/ios/project/WorkAI/Images.xcassets/*
-cp -rf ../WorkAI_Assets/* ~/build-workai/ios/project/WorkAI/Images.xcassets/
+rm -rf ~/build-sharpai/ios/project/sharpai/Images.xcassets/*
+cp -rf ../WorkAI_Assets/* ~/build-sharpai/ios/project/sharpai/Images.xcassets/
 
