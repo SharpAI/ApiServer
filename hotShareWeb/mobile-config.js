22fbf640297d34a7320930da28bc358f99797d78
sraita <guifubo@msn.cn>
2018-03-28 16:20:45 +0800

    Bump Version to 0.0.6 .

--- hotShareWeb/mobile-config.js
+++ hotShareWeb/mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   id: 'org.sharpai.everywhere',
-  version: '0.0.5',
+  version: '0.0.6',
   name: 'sharpai-web',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
