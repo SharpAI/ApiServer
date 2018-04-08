0d58f1e96129f1f88585bc61ad8fcc0e6c5705c3
wukai <wukai@wukaideMacBook.local>
2018-04-08 18:00:45 +0800

    Bump Version 0.0.9

--- hotShareWeb/mobile-config.js
+++ hotShareWeb/mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   id: 'org.sharpai.everywhere',
-  version: '0.0.8',
+  version: '0.0.9',
   name: 'sharpai-web',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
