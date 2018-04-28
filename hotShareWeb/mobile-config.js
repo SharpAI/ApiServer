7da77185a5d7cbc901f51d93e3a88ba03d809261
jihadwk <gogs@fake.local>
2018-04-28 09:35:47 +0800

    Bump version 1.0.25

--- hotShareWeb/mobile-config.js
+++ hotShareWeb/mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   id: 'org.sharpai.everywhere',
-  version: '1.0.24',
+  version: '1.0.25',
   name: 'sharpai-web',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
