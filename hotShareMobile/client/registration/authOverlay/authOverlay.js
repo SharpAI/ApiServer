15e41e7480513d5d6ed0945b51f20cde2db99072
Roger Ning <ningning-718@users.noreply.github.com>
2018-05-22 14:35:46 +0800

    turn off torch when scan barcode (#3184)

--- hotShareMobile/client/registration/authOverlay/authOverlay.js
+++ hotShareMobile/client/registration/authOverlay/authOverlay.js
@@ -94,7 +94,7 @@ if (Meteor.isClient) {
         preferFrontCamera: false, // iOS and Android
         showFlipCameraButton: true, // iOS and Android
         showTorchButton: true, // iOS and Android
-        torchOn: true, // Android, launch with the torch switched on (if available)
+        torchOn: false, // Android, launch with the torch switched on (if available)
         prompt: "Place a barcode inside the scan area", // Android
         resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
         formats: "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
