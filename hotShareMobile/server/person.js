5e7b0c676feb15a2fae63fb40848fa532b327d37
jihadwk <gogs@fake.local>
2018-05-22 16:01:57 +0800

    fix bug issues #3181

--- hotShareMobile/server/person.js
+++ hotShareMobile/server/person.js
@@ -1693,7 +1693,10 @@ Meteor.methods({
       0, 0, 0, 0);
 
     if (relation.app_user_id) {
-      workstatus = WorkStatus.findOne({'group_id': relation.group_id, 'app_user_id': relation.app_user_id, 'date': today_utc, app_user_id: relation.app_user_id});
+      workstatus = WorkStatus.findOne({'group_id': relation.group_id, 'app_user_id': relation.app_user_id, 'date': today_utc});
+    }
+    if (!workstatus && relation.person_name) {
+      workstatus = WorkStatus.findOne({'group_id': relation.group_id, 'person_name': relation.person_name, 'date': today_utc});
     }
     if (!workstatus) {
       WorkStatus.insert({
