8bf128da87d6c9d85b96d6ee24559c234f8f3c81
Roger <xning@actiontec.com>
2018-08-06 17:20:34 +0800

    Revert "tmp commit for jenkins build"
    This reverts commit 5bf8cb375d41781d13d287997e7cb6297e99b50c.

--- hotShareMobile/build.sh
+++ hotShareMobile/build.sh
@@ -4,9 +4,9 @@ rm -rf .meteor/local/cordova-build
 rm -rf .meteor/local/build
 rm -rf .meteor/local/bundler-cache
 rm -rf .meteor/local/plugin-cache
-#meteor build ~/build-sharpai --server=http://workaihost.tiegushi.com
+meteor build ~/build-sharpai --server=http://workaihost.tiegushi.com
 #测试版
-meteor build ~/build-sharpai --server=http://testworkai.tiegushi.com
+#meteor build ~/build-sharpai --server=http://testworkai.tiegushi.com
 rm -rf ~/build-sharpai/ios/project/sharpai/Images.xcassets/*
 cp -rf ../WorkAI_Assets/* ~/build-sharpai/ios/project/sharpai/Images.xcassets/
 
