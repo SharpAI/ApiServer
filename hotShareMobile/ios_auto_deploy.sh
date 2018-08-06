d5fa149017b9deefa9447ec85f078e75d167bd59
Roger <xning@actiontec.com>
2018-08-06 17:19:34 +0800

    update ios build script

--- hotShareMobile/ios_auto_deploy.sh
+++ hotShareMobile/ios_auto_deploy.sh
@@ -3,10 +3,10 @@
 cd ~/build-sharpai/ios/project
 
 cp ~/workspace/hotShare/sharpai-buildfiles/ios/Gemfile ./
-rsync -Rv  ~/workspace/hotShare/sharpai-buildfiles/ios/fastlane ./
-rsync -Rv ~/workspace/hotShare/sharpai-buildfiles/ios/CordovaLib ./
-rsync -Rv ~/workspace/hotShare/sharpai-buildfiles/ios/sharpai ./
-rsync -Rv ~/workspace/hotShare/sharpai-buildfiles/ios/sharpai.xcodeproj ./
+cp -R  ~/workspace/hotShare/sharpai-buildfiles/ios/fastlane ./
+cp -R ~/workspace/hotShare/sharpai-buildfiles/ios/CordovaLib ./
+cp -R ~/workspace/hotShare/sharpai-buildfiles/ios/sharpai ./
+cp -R ~/workspace/hotShare/sharpai-buildfiles/ios/sharpai.xcodeproj ./
 
 VER=`grep version_of_build ~/workspace/hotShare/hotShareMobile/lib/6_version.js | cut -d"'" -f 2`
 fastlane gym --export_method ad-hoc
