8c5c8e9c4140bc9f9c17322bcc4b2c659a515d8f
Roger <xning@actiontec.com>
2018-08-28 13:21:47 +0800

    Bump version to 1.0.66

--- hotShareWeb/lib/6_version.js
+++ hotShareWeb/lib/6_version.js
@@ -1,1 +1,1 @@
-version_of_build = '1.0.64';
+version_of_build = '1.0.66';
--- hotShareWeb/mobile-config.js
+++ hotShareWeb/mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   id: 'org.sharpai.everywhere',
-  version: '1.0.64',
+  version: '1.0.66',
   name: 'sharpai-web',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
