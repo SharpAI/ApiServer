d2c8f12738d5158820a1c16025f4cc56af6e53f5
Roger <xning@actiontec.com>
2018-09-20 13:24:33 +0800

    Bump version to 1.0.73

--- hotShareMobile/mobile-config.js
+++ hotShareMobile/mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   id: 'org.sharpai.everywhere',
-  version: '1.0.72',
+  version: '1.0.73',
   name: 'sharpai',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
