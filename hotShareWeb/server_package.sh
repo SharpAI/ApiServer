38e7568f9a1f98b4dccc19b63b804f97cae7eea5
Roger <xning@actiontec.com>
2018-09-26 18:21:53 +0800

    change CI server package target path

--- hotShareWeb/server_package.sh
+++ hotShareWeb/server_package.sh
@@ -8,5 +8,5 @@ docker build -t lambdazhang/raidcdn:sharpai-$VER .
 docker save -o sharpai-$VER.tar lambdazhang/raidcdn:sharpai-$VER
 gzip sharpai-$VER.tar
 TIMESTAMP=`date "+%Y%m%d%H%M%S"`
-mv sharpai-$VER.tar.gz ~/sharpai-$VER-$TIMESTAMP.tar.gz
+mv sharpai-$VER.tar.gz ~/.jenkins/userContent/sharpai/sharpai-$VER-$TIMESTAMP.tar.gz
 
