d2e272d8f9221c7e06ec2e81641d12a3b37630d6
Roger <xning@actiontec.com>
2018-09-26 15:09:11 +0800

    ios CI scripts change

--- hotShareMobile/ios_auto_deploy.sh
+++ hotShareMobile/ios_auto_deploy.sh
@@ -2,7 +2,7 @@
 
 cd ~/build-sharpai/ios/project
 
-PROJ_DIR=$HOME/workspace/hotShare
+PROJ_DIR=$HOME/workspace/sharpai
 cp $PROJ_DIR/sharpai-buildfiles/ios/Gemfile ./
 cp -R $PROJ_DIR/sharpai-buildfiles/ios/fastlane ./
 cp -R $PROJ_DIR/sharpai-buildfiles/ios/CordovaLib ./
@@ -10,6 +10,9 @@ cp -R $PROJ_DIR/sharpai-buildfiles/ios/sharpai ./
 cp -R $PROJ_DIR/sharpai-buildfiles/ios/sharpai.xcodeproj ./
 
 VER=`grep version_of_build $PROJ_DIR/hotShareMobile/lib/6_version.js | cut -d"'" -f 2`
+
+sed -i.bak "s/1.0.75/$VER/g" sharpai/sharpai-Info.plist
+
 fastlane gym --export_method ad-hoc
 TIMESTAMP=`date "+%Y%m%d%H%M%S"`
 DESTFILE="$HOME/sharpai-$VER-$TIMESTAMP.ipa"
