d696849699744c63670f043401d877f9c6be15fc
Simba Zhang <solderzzc@gmail.com>
2018-10-10 15:30:08 -0700

    To fixed a bug caused by GetData

--- DeepEyeMonitor/rt-portal/client/templates/realtime.js
+++ DeepEyeMonitor/rt-portal/client/templates/realtime.js
@@ -1,55 +1,6 @@
 GetData = function() {
   if(!document.getElementById("chart-id"))
     return;
-
-  Meteor.call('getChartData', Meteor.userId(), function(err, data) {
-    if(err) throw err;
-
-    if(!Session.get('currentPage') || Session.get('currentPage') != 'realtime')
-        return;
-
-    var chartData = getChartData();
-    var serverSentDate = new Date(data.ts)
-    console.log(serverSentDate)
-      var  interval=1;
-      var timeIndex={}
-      serverSentDate.setMinutes(serverSentDate.getMinutes() - 59);
-      for(var i=0;i<60;i++){
-          var ts=serverSentDate.getHours() + ':' + serverSentDate.getMinutes()
-          timeIndex[ts]=i
-          chartData.labels.push(ts);
-          chartData.datasets[0].data.push(0);
-          chartData.datasets[1].data.push(0);
-          chartData.datasets[2].data.push(0);
-          serverSentDate.setMinutes(serverSentDate.getMinutes() + 1);
-      }
-    var hasData = false
-    _.each(data.matrics, function(item) {
-      var date = new Date(item.time);
-      var timestamp=date.getHours() + ":" + date.getMinutes()
-      chartData.datasets[0].data[timeIndex[timestamp]]=(item.orig)>=0?Math.ceil(item.orig):0;
-      chartData.datasets[1].data[timeIndex[timestamp]]=(item.raid)>=0?Math.ceil(item.raid):0;
-      chartData.datasets[2].data[timeIndex[timestamp]]=(item.free)>=0?Math.ceil(item.free):0;
-      hasData=true
-    });
-
-    if(hasData){
-        Session.set('noTrafficData',false)
-        document.getElementById('chartTitle').innerHTML="Real Time Traffic (MB/Min)"
-    } else{
-        Session.set('noTrafficData',true)
-        document.getElementById('chartTitle').innerHTML="No Traffic data in an hour"
-    }
-    //var ctx = $("#chart-id").get(0).getContext("2d");
-    var ctx = document.getElementById("chart-id").getContext("2d");
-    var isFirstTime=true
-    if(typeof HeartBeatChart != "undefined") {
-      HeartBeatChart.destroy();
-      isFirstTime=false
-    }
-      HeartBeatChart = new Chart(ctx).Line(chartData,{responsive : true,animation: isFirstTime});
-      legend(document.getElementById("lineLegend"), chartData,HeartBeatChart);
-  });
 };
 function legend(parent, data) {
     legend(parent, data, null);
@@ -148,11 +99,11 @@ function getChartData() {
 }
 
 Template.realtime.rendered = function() {
-  document.getElementById('chartTitle').innerHTML="Loading data from server"
-  Session.set('noTrafficData','loading')
-  GetData();
-  realTimeInterval = window.setInterval(GetData, 60* 1000);
+  //document.getElementById('chartTitle').innerHTML="Loading data from server"
+  //Session.set('noTrafficData','loading')
+  //GetData();
+  //realTimeInterval = window.setInterval(GetData, 60* 1000);
 };
 Template.realtime.onDestroyed(function (){
-  realTimeInterval = window.clearInterval(realTimeInterval);
+  //realTimeInterval = window.clearInterval(realTimeInterval);
 });
