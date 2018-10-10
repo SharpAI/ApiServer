f99aff26555300a984683958a482d258ee0b9f21
Simba Zhang <solderzzc@gmail.com>
2018-10-09 17:31:38 -0700

    No need to build hotShareWeb-StaticJS

--- hotShareWeb/build_bundle.sh
+++ hotShareWeb/build_bundle.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cd ../hotShareWeb-StaticJS
-npm run build
-cd -
+#cd ../hotShareWeb-StaticJS
+#npm run build
+#cd -
 #git diff .
 rm -rf hotShareWeb.tar.gz
 meteor build --architecture=os.linux.x86_64 ../
 mv ../hotShareWeb.tar.gz ./
 
-echo "run \"docker build -t zhangzhiyue/hotshare:version .\""
+echo "run \"docker build -t lambdazhang/raidcdn:sharpai-version .\""
