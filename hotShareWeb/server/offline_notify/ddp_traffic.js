2de97708913e46e098ddd6891f2d527bcdc97f9d
Simba Zhang <solderzzc@gmail.com>
2018-10-11 18:18:57 -0700

    To set camera run info in Devices collection.

--- hotShareWeb/server/offline_notify/ddp_traffic.js
+++ hotShareWeb/server/offline_notify/ddp_traffic.js
@@ -14,6 +14,14 @@ Meteor.startup(function () {
       var peerData = peerCollection.findOne({clientID: info.clientID});
       info.updateBy = new Date();
       peerCollection.update({clientID:info.clientID},{$set:info},{upsert:true});
+
+      if(info && info.total_tasks && info.total_tasks > 0){
+        console.log('Camera connected')
+        Devices.update({uuid: info.clientID},{$set:{camera_run:true}})
+      } else if(info.total_tasks===0){
+        console.log('Camera disconnected')
+        Devices.update({uuid: info.clientID},{$set:{camera_run:false}})
+      }
     }
   })
 })
