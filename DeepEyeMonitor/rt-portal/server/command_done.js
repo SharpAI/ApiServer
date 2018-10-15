edc381336b187deddad88f955bb1753d11c82818
Hermit <327297038@qq.com>
2018-10-15 17:27:15 +0800

    update box config status (#3214)

--- DeepEyeMonitor/rt-portal/server/command_done.js
+++ DeepEyeMonitor/rt-portal/server/command_done.js
@@ -5,6 +5,9 @@ Meteor.startup(function () {
       console.log('command done: '+result);
       console.log('id is: '+id)
       Commands.update({_id: '' + id},{$set:{done:true,result:result}});
+      if (result && result.client_id) {
+        peerCollection.update({clientID: result.client_id},{$set:{'boxCfgServer.status': 'done'}},{upsert:true});
+      }
     }
   })
 })
