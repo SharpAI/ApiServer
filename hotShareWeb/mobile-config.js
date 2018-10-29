7e21ab910445df440a58e34f21dd3440627697ea
Roger <xning@actiontec.com>
2018-10-29 17:56:56 +0800

    Bump version to 1.1.2

--- hotShareWeb/mobile-config.js
+++ hotShareWeb/mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   id: 'org.sharpai.everywhere',
-  version: '1.1.1',
+  version: '1.1.2',
   name: 'sharpai-web',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
