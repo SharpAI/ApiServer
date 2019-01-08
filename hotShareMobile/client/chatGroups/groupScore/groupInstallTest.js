acaa8fa4e82757c75e3ef8a3c6c02da646e98b33
zhanghuanll <40556850+zhanghuanll@users.noreply.github.com>
2019-01-08 11:13:00 +0800

    注释新注册用户时的看一下介绍 (#3364)
    * 部署评测设备状态图标修复
    
    * 注释新注册用户时的看一下介绍

--- hotShareMobile/client/chatGroups/groupScore/groupInstallTest.js
+++ hotShareMobile/client/chatGroups/groupScore/groupInstallTest.js
@@ -65,12 +65,16 @@ Template.groupInstallTest.helpers({
             case 4:
                 head = '设备状态';
                 if (dev.online)
-                    deviceImg = '/device_online.png';
+                    deviceImg = '/face_box_online.svg';
+                else
+                    deviceImg = '/face_box_offline.svg';
                 if (dev.camera_run)
-                    cameraImg = '/camera_online.png';
+                    cameraImg = '/camera_online.svg';
+                else
+                    cameraImg = '/camera_offline.svg';
                 content = '<div><div style="margin: 10px 20px;">脸脸盒：<img src="' + deviceImg
-                 + '"></div><div style="margin: 10px 20px;">摄像头：<img src="' + cameraImg
-                 + '"></div><p style="margin: 10px 20px; color: red; text-align: center;">您的设备未接通，请检查设备连接后再次进行部署评测</p></div>';
+                + '" "'+ "width='30' height='20'" +'"></div><div style="margin: 10px 20px;">摄像头：<img src="' + cameraImg 
+                + '" "'+ "width='26' height='22'" +'"></div><p style="margin: 10px 20px; color: red; text-align: center;">您的设备未接通，请检查设备连接后再次进行部署评测</p></div>';
                 btn = "放弃";
                 showClose = 'display: none;';
                 break;
